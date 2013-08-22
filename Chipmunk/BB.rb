04dbbe326cd1b9bade267b3c9f4624ad1bd9374f
Raven <RavensKrag@gmail.com>
2013-08-21 23:07:54 -0400

    Create method to easy get corners of bounding box.

--- Chipmunk/BB.rb
+++ Chipmunk/BB.rb
@@ -12,6 +12,70 @@ module CP
 			CP::Vec2.new(self.l+width/2, self.b+height/2)
 		end
 		
+		def corners(start=:top_left, rotation=:clockwise)
+			# CW from top left
+			
+			# pattern
+			# 11	top left
+			# 10	top right
+			# 00	bottom right
+			# 01	bottom left
+			
+			# 1,1,0,0  truth value
+			# 1,2,3,4  index
+			
+			top, left =	case start
+							when :top_left
+								[1, 2]
+							when :top_right
+								[2, 3]
+							when :bottom_right
+								[3, 4]
+							when :bottom_left
+								[4, 1]
+						end
+			
+			
+			
+			output = Array.new(4)
+			output.each_index do |i|
+				x =	if left <= 2
+						self.l
+					else
+						self.r
+					end
+				
+				y =	if top <= 2
+						self.t
+					else
+						self.b
+					end
+				
+				output[i] = CP::Vec2.new(x,y)
+				
+				
+				x += 1
+				x = 1 if x > 4
+				
+				y += 1
+				y = 1 if y > 4
+			end
+			
+			
+			if rotation == :clockwise || rotation == :cw
+				# should already by cw
+			elsif rotation == :clockwise || rotation == :anticlockwise || rotation == :ccw
+				# reverse winding
+				output = [
+					output[0], output[3], output[2], output[1]
+				]
+			else
+				raise "Error: Invalid symbol for rotation"
+			end
+			
+			return output
+		end
+		
 		def height
 			self.t - self.b
 		end
