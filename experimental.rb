92b55c54171564950d361fc76d35ce4275f5f46a
Raven <RavensKrag@gmail.com>
2013-09-03 18:23:52 -0400

    Experiment with ways of describing clicking on objects vs empty space.

--- experimental.rb
+++ experimental.rb
@@ -68,9 +68,120 @@ end
 # but that's not realistic.  Most actions need to be spread out between
 # different types of mouse events
 # -----
+@mouse.selection_type :object_get do |space, selection, position_vector|
+	# trigger on empty space
+	obj = space.object_at position_vector
+	
+	return obj == nil
+end
+
+@mouse.selection_type :empty_space do |space, selection, position_vector|
+	# trigger on empty space
+	obj = space.object_at position_vector
+	
+	return obj == nil
+end
+
 
 @mouse.event :select_single do
 	bind_to Gosu::MsLeft
+	
+	# TODO: need way to say if event should fire on blank, or when there's an object under the cursor
+	# (allows for more complex event signatures, which makes it easier for the system to detect collision, instead of forcing the user to do it)
+	
+	fire :always
+	fire :on_blank
+	fire :on_object_get
+	fire_on :any
+	fire_on :all # for what events should we fire? fire on all events
+	# how would :any and :all be different? should they just be "aliases" of the same command?
+	fire_on :blank # no objects under cursor
+	fire_on :hover # there's an object under the cursor, we haven't tried to pick yet
+	fire_on :object_get
+	
+	fire_on :selction_active # one or more objects have already been selected
+	fire_on :selction_empty # nothing currently selected
+	
+	
+	launch_when :over_object
+	launch_when_over :space
+	launch_when_over :object
+	
+	# If block returns true, then events will fire
+		# alternatively: return addition to selection or nil
+			# means to clear current overall selection as well?
+	# only checked for button down transition
+	# after that, the rest of the cycle is guaranteed to complete
+	select do |space, selection, position_vector|
+		# trigger on empty space
+		obj = space.object_at position_vector
+		
+		return obj == nil
+	end
+	
+	
+	
+	
+	# if the logic for mouse picking is going to be placed outside of these callbacks, need way to specifying what the logic is
+	select :optimal
+	select :all
+	select :highest
+	select :lowest
+	select :largest_area
+	select :smallest_area
+	# allowing for multiple statements is more expressive than just saying "optimal"
+	select :highest, :largest_area
+	
+	
+	
+	
+	pick :object
+		pick :object, :optimal
+		pick :object, :highest, :largest_area
+		
+		pick :object do |objects|
+			# objects - all objects in space under the mouse
+			# return value of block is which objects to add to selection
+			
+			
+			# within a certain margin of the smallest object
+			# largest area, closest to mouse
+		end
+		
+		
+		
+		
+		
+		# works similar to array methods like #select
+		# iterates over all objects under the mouse
+		# each and every object is passed into the block
+		# if the block returns true, then that object is added to the selection
+		pick :object do |objects|
+			# objects - all objects in space under the mouse
+			# return value of block is which objects to add to selection
+		end
+		
+		
+		# works similar to Array#sort
+		# iterates over all objects under the mouse
+		# block evaluates comparison between a and b
+		# if there is only one object under the mouse, it is automatically added to the selection
+		pick :object do |a, b|
+			
+		end
+		
+		
+		
+		pick :object do |objects, selection|
+			
+		end
+		
+	pick :point do |vector|
+		# vector is in world space
+	end
+	
+	
+	
 	
 	click do |selection|
 		# get object under cursor
