8ac0b1193869ccf615d5945a543107c870080a68
Raven <RavensKrag@gmail.com>
2014-01-21 03:59:34 -0500

    Create class to profile execution time of a block of code.

---
+++ lib/PACKAGE_NAME/utilities/PerformanceTimer.rb
@@ -0,0 +1,30 @@
+# Measure how long it takes to complete the operation specified in the given block
+
+module Metrics
+	class Timer
+		attr_reader :dt
+		
+		def initialize(task_name)
+			@task_name = task_name
+			
+			
+			timer_start = Gosu::milliseconds
+			
+			yield
+			
+			timer_end = Gosu::milliseconds
+			@dt = timer_end - timer_start
+
+			puts self.to_s
+		end
+		
+		def to_s
+			"#{@task_name.to_s.upcase} COMPLETE (#{@dt} ms)"
+		end
+	end
+end
+
+# API example
+# Metrics::Timer.new "loading files" do
+# 	puts "Doing some stuff here"
+# end
