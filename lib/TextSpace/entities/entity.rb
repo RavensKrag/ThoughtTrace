52526461d81bb1af0d32de2d0a36eff14b614f37
Raven <RavensKrag@gmail.com>
2014-02-07 00:28:04 -0500

    Sketch ideas for replacing actions in Entity and multi-dependency errors.

--- lib/TextSpace/entities/entity.rb
+++ lib/TextSpace/entities/entity.rb
@@ -1,8 +1,15 @@
 class Entity
 	def initialize
 		@components = Hash.new
-		
 		@actions = Hash.new
+	end
+	
+	def update
+		
+	end
+	
+	def draw
+		
 	end
 	
 	
@@ -43,151 +50,83 @@ class Entity
 		end
 		
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		# # select all components that could not be resolved into real variables
+		# unresolved_dependencies = {}.select{ |k,v| v == nil }
+		# unless unresolved_dependencies.empty?
+		# 	raise "Components #{unresolved_dependencies.join(', ')} are required by #{action_class} in #{self}"
+		# 	raise "#{action_class} in #{self} needs missing components: #{unresolved_dependencies.join(', ')}"
+		# end
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		if @actions.has_key? action_class.name
+			# # Dependencies should be good to go, but need to replace the old action
+			# # (is it safe to say the dependencies are the same?)
+			
+			# # Iterate over all actions which list the desired action as a dependency
+			# @actions.select{ |name, action| action.class.actions.include? action }
+			# .each do |action|
+			# 	action.actions[action_class.name] = 
+			# end
+			
+			
+			
+			
+			
+			# @actions.select{ |name, action| 
+			# 	action.class.dependencies[:actions].include? action_class.name 
+			# }.each do |name, action|
+			# 	action.actions[action_class.name] = 
+			# end
+		end
+		
+		
+		
+		
 		# TODO: Change the way you get names out of an action class. This will return the string form of the class's name, which is not the "name" of the action, necessarily.
+		
+		# NOTE: This method of passing components and actions means that if a different action is bound to the Entity at a later time, the change will not be reflected.  Consider just passing directly the @components and @actions variables.
+		# (But that has it's own problems, as you don't want people outside of the Entity to be able to add/remove from those collections.  You just want them to have the latest updates.)
 		@actions[action_class.name] = action_class.new(components, actions)
-		
-		return nil # prevent leaking of the @actions collection
-		# consider returning this Entity instead?
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		components = action.components.collect do |component_name|
-			component = @components[component_name]
-			
-			if component.nil?
-				raise "Component #{component_name} required by #{action} in #{self.class}"
-			end
-			
-			component
-		end
-		
-		actions = action.actions.collect do |action_name|
-			sub_action = @actions[action_name]
-			
-			if sub_action.nil?
-				raise "Action #{component_name} required by #{action} in #{self.class}"
-			end
-			
-			sub_action
-		end
-		
-		
-		
-		@actions[action.name] = self.new(
-				action.name, 
-				Hash[action.components.zip components],
-				Hash[action.actions.zip actions]
-		)
-		
-		return nil # prevent leaking of the @actions collection
-		# consider returning this Entity instead?
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		components = action.components.inject(Hash.new) do |group, component_name|
-			component = @components[component_name]
-			
-			if component.nil?
-				raise "Component #{component_name} required by #{action} in #{self.class}"
-			end
-			
-			group[component_name] = component
-			group
-		end
-		
-		actions = action.actions.inject(Hash.new) do |group, action_name|
-			action = @actions[action_name]
-			
-			if action.nil?
-				raise "Action #{component_name} required by #{action} in #{self.class}"
-			end
-			
-			group[action_name] = action
-			group
-		end
-		
-		
-		
-		@actions << self.new(action.name, components, actions)
 		
 		return nil # prevent leaking of the @actions collection
 		# consider returning this Entity instead?
 	end
 	
-	def add_component(component)
+	def add_component(component_class)
 		# require dependencies to be added first
 		# If an component is added before any of the components it depends on, throw exception.
 		
 		
-		
-		components = action.components.collect do |component_name|
+		components = component_class.components.inject(Hash.new) do |group, component_name|
 			component = @components[component_name]
 			
 			if component.nil?
-				raise "Component #{component_name} required by #{action} in #{self.class}"
-			end
-			
-			component
-		end
-		
-		
-		
-		@actions << self.new(
-				action.name, 
-				Hash[action.components.zip components],
-				Hash[action.actions.zip actions]
-		)
-		
-		return nil # prevent leaking of the @actions collection
-		# consider returning this Entity instead?
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		components = action.components.inject(Hash.new) do |group, component_name|
-			component = 
-			
-			if component.nil?
-				raise "Component #{component_name} required by #{action} in #{self.class}"
+				raise "Component #{component_name} required by #{action_class} in #{self}"
 			end
 			
 			group[component_name] = component
 			group
 		end
 		
-		@actions << self.new(action.name, components)
+		
+		@components[component_class.name] = self.new(components)
 		
 		return nil # prevent leaking of the @actions collection
 		# consider returning this Entity instead?
