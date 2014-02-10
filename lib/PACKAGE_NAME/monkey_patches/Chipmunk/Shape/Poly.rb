c90d7d44c6bd309ca452a2548d469e3469c423e8
Raven <RavensKrag@gmail.com>
2014-02-10 04:30:37 -0500

    Bring over old Chipmunk monkeypatches.

---
+++ lib/PACKAGE_NAME/monkey_patches/Chipmunk/Shape/Poly.rb
@@ -0,0 +1,52 @@
+module CP
+	module Shape
+		class Poly
+			# Draw poly from verts in world space coordinates
+			# Assumes that the poly is convex whole, but that's an assumption built into Chipmunk.
+			def draw(color, z=0)
+				$window.gl z do
+					glBegin(GL_TRIANGLE_FAN)
+						glColor4ub(color.red, color.green, color.blue, color.alpha)
+						
+						# TODO: Convert coordinates on GPU using local coordinates and transform
+						# transform should account for translation and rotation
+						self.each_vert do |v|
+							vec = self.body.local2world(v)
+							
+							glVertex2f(vec.x, vec.y)
+						end
+					
+					glEnd()
+				end
+			end
+			
+			# return an array of all vertices
+			def verts
+				out = Array.new(self.num_verts)
+				self.num_verts.times do |i|
+					out[i] = self.vert(i)
+				end
+				
+				return out
+			end
+			
+			def area
+				CP.area_for_poly self.verts
+			end
+			
+			
+			
+			include Enumerable
+			
+			def each
+				self.num_verts.times do |i|
+					yield self.vert(i)
+				end
+			end
+			
+			alias :each_vert :each
+			alias :each_vert_with_index :each_with_index
+			alias :each_vert_with_object :each_with_object
+		end
+	end
+end
---
+++ lib/PACKAGE_NAME/monkey_patches/Chipmunk/Vec2.rb
@@ -0,0 +1,53 @@
+module CP
+	class Vec2
+		class << self
+			# Return the angle between two vectors
+			def angle_between(v1, v2)
+				# normalization of zero vectors results in NaN, which is really bad
+				v1 = v1.normalize unless v1.zero?
+				v2 = v2.normalize unless v2.zero?
+				
+				cos = v1.dot v2
+				
+				theta = Math::acos(cos)
+				
+				return theta
+			end
+		end
+		
+		def zero?
+			return self.x == 0 && self.y == 0
+		end
+		
+		def to_screen_space
+			return self - $window.camera.offset
+		end
+		
+		def to_world_space
+			return self + $window.camera.offset
+		end
+		
+		def clone
+			return CP::Vec2.new(self.x, self.y)
+		end
+		
+		
+		include TextSpace::Serializable
+		
+		def init_with coder
+			args = YAML.load(coder.scalar)
+			initialize(*args)
+		end
+
+		def to_string_representation
+			[self.x, self.y].to_yaml
+		end
+		
+		class << self
+			def from_string_representation(string_representation)
+				args = YAML.load(string_representation)
+				new(*args)
+			end
+		end
+	end
+end
