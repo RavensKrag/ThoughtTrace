a6e00781e10e68de7e801ecb1182135ad5df042b
Raven <RavensKrag@gmail.com>
2014-02-19 20:02:58 -0500

    Connect Physics component and Move action to Text entity.

--- lib/TextSpace/entities/components/physics.rb
+++ lib/TextSpace/entities/components/physics.rb
@@ -4,6 +4,23 @@ module TextSpace
 
 class Physics < Component
 	interface_name :physics
+	
+	def initialize(parent, body, shape)
+		# super() # Set up state machine
+		
+		@body = body
+		@shape = shape
+		@shape.obj = parent
+	end
+	
+	def update
+		
+	end
+	
+	# Render collision geometry
+	def draw(color, z=0)
+		@shape.draw color, z
+	end
 end
 
 
--- lib/TextSpace/entities/text.rb
+++ lib/TextSpace/entities/text.rb
@@ -7,17 +7,18 @@ class Text < Entity
 	def initialize(font)
 		super()
 		
-		# 				body = CP::Body.new(Float::INFINITY, Float::INFINITY) 
-		# 				shape = CP::Shape::Poly.new body, new_geometry(), CP::Vec2.new(0,0)
-		# add_component	TextSpace::Components::Physics.new self, body, shape
-		
-		
 		@font = font
-		
-		
 		
 		@height = 30
 		@color = Gosu::Color.argb(0xffFFFFFF)
+		
+		
+						body = CP::Body.new(Float::INFINITY, Float::INFINITY) 
+						shape = CP::Shape::Poly.new body, new_geometry(), CP::Vec2.new(0,0)
+		add_component	TextSpace::Components::Physics.new self, body, shape
+		
+		
+		add_action TextSpace::Actions::Move.new
 	end
 	
 	def update
@@ -34,6 +35,28 @@ class Text < Entity
 		@font.draw	@string, @height,
 					0,0, z_index, # position relative to top left corner of text
 					@color
+	end
+	
+	
+	private
+	
+	def new_geometry
+		l = 0
+		b = 0
+		r = @font.width(@string, @height)
+		t = @height
+		
+		# cw winding
+		verts = [
+			CP::Vec2.new(l, t),
+			CP::Vec2.new(r, t),
+			CP::Vec2.new(r, b),
+			CP::Vec2.new(l, b)
+		]
+		
+		raise "Problem with specified verts." unless CP::Shape::Poly.valid? verts
+		
+		return verts
 	end
 end
 
