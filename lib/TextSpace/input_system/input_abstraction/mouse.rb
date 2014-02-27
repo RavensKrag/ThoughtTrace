30781e28daef61c103cde25c26b203398a8574be
Raven <RavensKrag@gmail.com>
2014-02-27 18:24:06 -0500

    Bring in mouse position code from MARK III build

--- lib/TextSpace/input_system/input_abstraction/mouse.rb
+++ lib/TextSpace/input_system/input_abstraction/mouse.rb
@@ -6,6 +6,20 @@ class Mouse
 	def initialize
 		
 	end
+	
+	def world_position
+		return CP::Vec2.new($window.mouse_x, $window.mouse_y).to_world_space
+	end
+	
+	def screen_position
+		return CP::Vec2.new($window.mouse_x, $window.mouse_y)
+	end
+	
+	alias :position_in_world :world_position
+	alias :position_in_world_coordinates :world_position
+	
+	alias :position_on_screen :screen_position
+	alias :position_in_screen_coordinates :screen_position
 end
 
 
