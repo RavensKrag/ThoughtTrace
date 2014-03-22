d714fd9ca0638201fc0e3e6445cc30f79f3aa241
Raven <RavensKrag@gmail.com>
2014-03-22 07:40:50 -0400

    Update cache of complied load / dump files.

--- lib/TextSpace/serialization/compiled_files/Text_dump.rb
+++ lib/TextSpace/serialization/compiled_files/Text_dump.rb
@@ -1,26 +1,26 @@
-class Text
-	class << self
+module TextSpace
+	class Text
+		class << self
 
 
 def dump(text)
-	# examining this object: text
-
-
-	text.resize!
-	height = text[:style][:height]
-
-	y = text[:physics].body.p.y
-	x = text[:physics].body.p.x
-
-	string = text.string
-
-	font = text.font
-	font_name = font.font_name
-
+	font = TextSpace::Font.new font_name
+	text = TextSpace::Text.new font
+	
+	text.string = string
+	
+	text[:physics].body.p.x = x
+	text[:physics].body.p.y = y
+	
+	text[:style][:height] = height
+	text.resize! # this only needs to be run when loading
+	# need to figure out how to resize automatically on font size change ASAP.strip_comments.flip
+	
 	return font_name, x, y, height, string
 end
 
 
 
+end
 end
 end
