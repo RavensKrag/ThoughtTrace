af0453d51c0fccfc7e50b48824e7729f11e1ca49
Raven <RavensKrag@gmail.com>
2014-03-22 06:34:58 -0400

    Load template into memory all at once, and then close the file handle.

--- lib/TextSpace/serialization/rakefile.rb
+++ lib/TextSpace/serialization/rakefile.rb
@@ -1,7 +1,24 @@
+require 'rubygems'
+
 require 'rake/clean'
+
+
+
 
 # ascii fonts from http://patorjk.com/software/taag/#p=display&f=Graceful&t=Constants
 # (could possibly just query that, actually)
+
+
+#  ____  ____  ____  _  _  ____ 
+# / ___)(  __)(_  _)/ )( \(  _ \
+# \___ \ ) _)   )(  ) \/ ( ) __/
+# (____/(____) (__) \____/(__)  
+path_to_this_file = File.expand_path '..', __FILE__
+Dir.chdir path_to_this_file
+
+require 'require_all'
+require_all './build_system'
+
 
 
 #   ___  __   __ _  ____  __  ___ 
@@ -15,15 +32,8 @@ LOAD_FILE_SUFFIX = '_load.rb'
 DUMP_FILE_SUFFIX = '_dump.rb'
 
 SOURCE_DIRECTORY = './source'
-OUTPUT_DIRECTORY = './complied_files'
+OUTPUT_DIRECTORY = './compiled_files'
 
-
-#  ____  ____  ____  _  _  ____ 
-# / ___)(  __)(_  _)/ )( \(  _ \
-# \___ \ ) _)   )(  ) \/ ( ) __/
-# (____/(____) (__) \____/(__)  
-path_to_this_file = File.expand_path '..', __FILE__
-Dir.chdir path_to_this_file
 
 
 #  ____  __   ____  __ _  ____ 
@@ -50,68 +60,65 @@ task :build do
 	Dir["#{SOURCE_DIRECTORY}/*.rb"].each do |source|
 		name = source.strip_extension
 		
-		{
-			:load => ['./templates/load.rb', LOAD_FILE_SUFFIX]
+		config = {
+			:load => ['./templates/load.rb', LOAD_FILE_SUFFIX],
 			:dump => ['./templates/dump.rb', DUMP_FILE_SUFFIX]
-		}.each do |config_name, data|
+		}
+		config.each do |config_name, data|
 			template_file, suffix = data
 			
 			
-			File.open(template_file, 'r') do |template|
-				
-				output_filename = "#{name}#{suffix}"
-				filepath = File.expand_path File.join(OUTPUT_DIRECTORY, output_filename)
-				
-				
+			# ACTIVE FILE HANDLES:
+			# source    -----	raw source file
+			
+			
+			# =================================
+			# =========== Procedure ===========
+			# =================================
+			# load file
+			# perform necessary operations
+			# and then perform one write pass
+			# =================================
+			
+			
+			# --- load file into memory
+			# copy entire template file into memory for editing
+			template = File.open(template_file, 'r')
+			lines = template.readlines
+			template.close
+			
+			
+			# --- filling out fields
+			# substitute CLASS_NAME for proper name of class
+			# 	name should be derived from name of source file
+			
+			
+			
+			# --- basic replacement
+			# substitute ARGS and OBJECT with proper values
+			# 	requires parsing of the source for ARGS and OBJECT values
+			
+			
+			
+			# --- body compilation
+			# perform necessary transforms on BODY
+			# 	must extract BODY code from source file,
+			# 	and then apply transforms defined in template
+			
+			
+			
+			output_filename = "#{name}#{suffix}"
+			filepath = File.expand_path File.join(OUTPUT_DIRECTORY, output_filename)
+			
+			# raise "no file found" unless File.exists? filepath
+			
+			File.open(filepath, 'w') do |out|
 				# ACTIVE FILE HANDLES:
 				# source    -----	raw source file
-				# template  -----	mold to base the construction of the output file on
+				# out       -----	compiled file
 				
-				
-				# =================================
-				# =========== Procedure ===========
-				# =================================
-				# load file
-				# perform necessary operations
-				# and then perform one write pass
-				# =================================
-				
-				
-				
-				# --- filling out fields
-				# substitute CLASS_NAME for proper name of class
-				# 	name should be derived from name of source file
-				
-				
-				
-				# --- basic replacement
-				# substitute ARGS and OBJECT with proper values
-				# 	requires parsing of the source for ARGS and OBJECT values
-				
-				
-				
-				# --- body compilation
-				# perform necessary transforms on BODY
-				# 	must extract BODY code from source file,
-				# 	and then apply transforms defined in template
-				
-				
-				
-				
-				
-				
-				
-				File.new(filepath, 'w') do |out|
-					# ACTIVE FILE HANDLES:
-					# source    -----	raw source file
-					# template  -----	mold to base the construction of the output file on
-					# out       -----	compiled file
-					
-					
-				end
 				
 			end
-			
 		end
 	end
 end
