7cd6b35423694c7293acd1c21011645220eb8dde
Raven <RavensKrag@gmail.com>
2014-03-22 02:17:45 -0400

    Sketch of serialization in mixin form using Array#pack.

---
+++ lib/TextSpace/entities/mixins/serializable.rb
@@ -0,0 +1,35 @@
+module TextSpace
+	module Serializable
+
+
+def foo
+	
+end
+
+# Figure out Array#pack pack string based on object types
+def pack(*args)
+	pack_string = ""
+	
+	args.each do |data|
+		pack_string <<	if data.is_a? Float
+							# double-precision, network (big-endian) byte order
+							'G'
+						elsif data.is_a? Integer
+							case data.size
+								# size starts at 8 and increases in steps of 4
+								when 8
+								when 12
+								when 16
+								when 32
+							end
+								
+						elsif data.is_a? String
+							
+						end
+	end
+end
+
+
+	
+end
+end
