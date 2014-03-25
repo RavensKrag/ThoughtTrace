b25da19b4cdb18238f89b2853559de5b5c782399
Raven <RavensKrag@gmail.com>
2014-03-25 11:34:33 -0400

    TYPO: Rectangle serialization called body by hash interface instead of method access.

--- lib/ThoughtTrace/serialization/source/Rectangle.rb
+++ lib/ThoughtTrace/serialization/source/Rectangle.rb
@@ -4,8 +4,8 @@ BODY {
 
 rectangle = ThoughtTrace::Rectangle.new width, height
 
-rectangle[:physics][:body].p.x = x
-rectangle[:physics][:body].p.y = y
+rectangle[:physics].body.p.x = x
+rectangle[:physics].body.p.y = y
 
 }
 
