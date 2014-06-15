443db82f8b7bc105481cb899ba4790f63b5119e0
Raven <RavensKrag@gmail.com>
2014-06-14 21:13:11 -0400

    Clearly define difference between #instance_eval and #class_eval for method definition.

--- lib/ThoughtTrace/serialization/templates/pack.rb
+++ lib/ThoughtTrace/serialization/templates/pack.rb
@@ -6,6 +6,14 @@
 # so that way you don't have to know class hierarchy
 # of the thing you're trying to add methods to
 
+# defining instance method
+	# class_eval code is like writing something inside a class definition
+	# instance_eval evaluates within the caller's scope
+	
+	# defining methods on an object attaches them to the meta of that object
+	# because an object can't have methods,
+	# only a class can have methods
+
 klass = ThoughtTrace.const_get(:CLASS_NAME)
 klass.class_eval do
 
--- lib/ThoughtTrace/serialization/templates/unpack.rb
+++ lib/ThoughtTrace/serialization/templates/unpack.rb
@@ -6,9 +6,16 @@
 # so that way you don't have to know class hierarchy
 # of the thing you're trying to add methods to
 
+# defining class method
+	# class_eval code is like writing something inside a class definition
+	# instance_eval evaluates within the caller's scope
+	
+	# defining methods on an object attaches them to the meta of that object
+	# because an object can't have methods,
+	# only a class can have methods
+
 klass = ThoughtTrace.const_get(:CLASS_NAME)
 klass.instance_eval do
-self.instance_eval do
 
 def unpack(ARGS)
 
@@ -21,6 +28,5 @@ return OBJECT
 
 
 
-end
 end
 end
