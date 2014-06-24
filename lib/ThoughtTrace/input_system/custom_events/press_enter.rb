0d9472f5693a54fa408ffbc419915b1fb25ab82c
Raven <RavensKrag@gmail.com>
2014-06-24 10:33:33 -0400

    Only splinter Text when 'enter' is pressed if the caret is somewhere in the middle.

--- lib/ThoughtTrace/input_system/custom_events/press_enter.rb
+++ lib/ThoughtTrace/input_system/custom_events/press_enter.rb
@@ -18,40 +18,53 @@ class PressEnter
 			# put new object in space
 			# move new object to proper position
 			
-			old_text = @text_input.text
-			
-			
-			
-			font = old_text.font
-			height = old_text.height
-			
-			
-			# --- splinter off part of string
-			str = old_text.string
 			i = @text_input.caret_index
-			main, splinter = str.slice!(0..i-1), str
-			# first i characters, rest of the string
-			# ie) up to and including index i, everything after that
 			
-			@text_input.text.string = main
-			
-			new_text = ThoughtTrace::Text.new font, splinter
-			new_text.height = height
-			
-			
-			
-			new_text[:physics].body.p = old_text[:physics].body.p.clone
-			new_text[:physics].body.p.y += height
-			# TODO: coordinate system - should be subtraction, eventually. Really would like -y to be down the screen
-			
-			
-			@space.entities.add new_text
-			
-			
-			
-			
-			@text_input.clear
-			@text_input.add new_text, 0
+			if i == 0
+				# at the start of the string
+				# creating a new Text would be a little silly
+				
+			elsif i == @text_input.text.string.size
+				# at the end of the string
+				# if you created a new text object, it would be empty
+			else
+				# somewhere in the middle
+				# split the Text object into multiple parts
+				
+				old_text = @text_input.text
+				
+				
+				
+				font = old_text.font
+				height = old_text.height
+				
+				
+				# --- splinter off part of string
+				str = old_text.string
+				main, splinter = str.slice!(0..i-1), str
+				# first i characters, rest of the string
+				# ie) up to and including index i, everything after that
+				
+				@text_input.text.string = main
+				
+				new_text = ThoughtTrace::Text.new font, splinter
+				new_text.height = height
+				
+				
+				
+				new_text[:physics].body.p = old_text[:physics].body.p.clone
+				new_text[:physics].body.p.y += height
+				# TODO: coordinate system - should be subtraction, eventually. Really would like -y to be down the screen
+				
+				
+				@space.entities.add new_text
+				
+				
+				
+				
+				@text_input.clear
+				@text_input.add new_text, 0
+			end
 		end
 	end
 	
