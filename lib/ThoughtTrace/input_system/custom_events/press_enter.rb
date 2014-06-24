b162b2ca18b8b9cace33258d3c82b21c696c03b0
Raven <RavensKrag@gmail.com>
2014-06-24 10:45:26 -0400

    Update comments and code clarity.

--- lib/ThoughtTrace/input_system/custom_events/press_enter.rb
+++ lib/ThoughtTrace/input_system/custom_events/press_enter.rb
@@ -11,12 +11,10 @@ class PressEnter
 	
 	def press
 		if @text_input.active?
-			# copy properties from existing text object
-			# make new text object
-			# put "overflow" portion of string into new text object
-			# remove "overflow" from original text object
-			# put new object in space
-			# move new object to proper position
+			# Effect similar to inserting a newline in a text editor
+			# create new text objects as necessary
+			
+			# TODO: coordinate system - make sure that -y is down the screen, update position shifts accordingly.
 			
 			i = @text_input.caret_index
 			
@@ -24,13 +22,29 @@ class PressEnter
 				# at the start of the string
 				# creating a new Text would be a little silly
 				
+				# might as well move the Text though, for a consistent mental model
+				old_text = @text_input.text
+				
+				old_text[:physics].body.p.y += old_text.height
 			elsif i == @text_input.text.string.size
 				# at the end of the string
 				# if you created a new text object, it would be empty
+				
+				
+				# do nothing
+				return
 			else
 				# somewhere in the middle
 				# split the Text object into multiple parts
 				
+				
+				# copy properties from existing text object
+				# make new text object
+				# put "overflow" portion of string into new text object
+				# remove "overflow" from original text object
+				# put new object in space
+				# move new object to proper position
+				# put the new object into the input system, instead of the old one
 				old_text = @text_input.text
 				
 				
@@ -45,23 +59,22 @@ class PressEnter
 				# first i characters, rest of the string
 				# ie) up to and including index i, everything after that
 				
-				@text_input.text.string = main
+				
+				# --- put attributes in the proper places
+				old_text.string = main
 				
 				new_text = ThoughtTrace::Text.new font, splinter
 				new_text.height = height
 				
 				
-				
+				# --- position the new object within the space
 				new_text[:physics].body.p = old_text[:physics].body.p.clone
 				new_text[:physics].body.p.y += height
-				# TODO: coordinate system - should be subtraction, eventually. Really would like -y to be down the screen
-				
 				
 				@space.entities.add new_text
 				
 				
-				
-				
+				# --- reconnect input buffer
 				@text_input.clear
 				@text_input.add new_text, 0
 			end
