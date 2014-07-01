0900c807e9bec3177bd5f17087e34988dca23e6c
Raven <RavensKrag@gmail.com>
2014-07-01 18:40:57 -0400

    Implement proper cloning for Entity objects by using serialization core.

--- lib/ThoughtTrace/cloning/entities/entity.rb
+++ lib/ThoughtTrace/cloning/entities/entity.rb
@@ -3,7 +3,17 @@ module ThoughtTrace
 
 class Entity
 	def clone
-		@components
+		# could easily just use the serialization facilities
+		# to generate a new Entity with the same properties as an existing one
+		# would be similar to duplicating a line in the save file.
+		# But because of how dump / load are implemented
+		# (independent of disk write/read format)
+		# it's easy to do the entire process in memory
+		
+		data = self.dump
+		other = self.class.load(*data)
+		
+		return other
 	end
 end
 
