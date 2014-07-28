9539261065a02d99ec129014450c6493a7d1b47a
Raven <RavensKrag@gmail.com>
2014-07-28 09:28:06 -0400

    Start testing out style serialization.

---
+++ experimental/style_system/serialization_test.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'gosu'
+
+
+
+path_to_file = File.expand_path(File.dirname(__FILE__))
+
+path = File.join '.', '..', '..'
+path_to_root = File.expand_path path, path_to_file
+
+puts path_to_root
+
+
+Dir.chdir File.join path_to_root, 'lib', 'ThoughtTrace', 'style' do
+	require './cascade'
+	require './pallet'
+	require './style'
+end
+
+Dir.chdir File.join path_to_root, 'experimental', 'style_system' do
+	require './color'
+end
+
+
+
+
+color = Gosu::Color.argb 0xffAABBCC
+data = color.pack
+puts data.to_s 16
+
+
+
+style = ThoughtTrace::Style::StyleObject.new
+style[:color] = color
+
+data = style.pack
+p data
