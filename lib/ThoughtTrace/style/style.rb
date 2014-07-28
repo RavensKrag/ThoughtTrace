c28aa69a534728b4622a82704ef716217132bdcb
Raven <RavensKrag@gmail.com>
2014-07-27 22:17:36 -0400

    Implement a way to serialize style values that are not basic Ruby types.

--- lib/ThoughtTrace/style/style.rb
+++ lib/ThoughtTrace/style/style.rb
@@ -10,14 +10,80 @@ class StyleObject
 	
 	
 	def pack
-		return @properties
+		hash = Hash.new
+		@properties.collect do |property, value|
+			hash[k] = pack_entry(value)
+		end
+		
+		return hash
 	end
 	
-	class << self
-		def unpack(args)
+	
+	# pack objects which are "complex"
+	# ie, they aren't part of the basic types YAML supports
+	def pack_entry(value)
+		if value.respond_to? :pack
+			# pack up the special data as necessary
+			# (should have the format "class<arg, arg, ..., arg>" when complete)
+			data = data.pack
+			data = data.join(', ') if data.is_a? Array
 			
+			return "#{value.class}<#{data}>"
+		else
+			# this data should just work. gonna just leave it alone
+			return value
 		end
 	end
+	private :pack_entry
+	
+	
+	
+	class << self
+		def unpack(property_hash)
+			obj = self.new
+			
+			
+			property_hash.each do |property, value_data|
+				value = 
+					if value_data.is_a? String
+						unpack_entry(value_data)
+					else
+						value
+					end
+				
+				
+				obj[property] = value
+			end
+			
+			
+			return obj
+		end
+		
+		
+		# string may be special data in the form of
+		# "class<arg, arg, ..., arg>"
+		# (in which case it needs to be unpacked into "real" data)
+		# or it could just be a standard string
+		def unpack_entry(string)
+			exp = /(.*)\<(.*)\>/
+			matchdata = string.match exp
+			
+			if matchdata
+				# found specially encoded data
+				klass = matchdata[1]
+				args  = matchdata[2].strip!.split(/\s*,\s*/)
+				
+				return klass.unpack(*args)
+			else
+				# it's actually just a normal string
+				return string
+			end
+		end
+		private :unpack_entry
+	end
+	
+	
+	
 end
 
 
