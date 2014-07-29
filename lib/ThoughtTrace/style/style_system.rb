b0ec5ce237b3d93ae06aaad11f78c5953db0293b
Raven <RavensKrag@gmail.com>
2014-07-28 20:35:26 -0400

    Adjust spacing for better flow, and remove cruft.

--- lib/ThoughtTrace/style/style_system.rb
+++ lib/ThoughtTrace/style/style_system.rb
@@ -65,7 +65,6 @@ class StyleSystem
 			
 			
 			
-			
 			pallets.each do |name, data|
 				pallet = ThoughtTrace::Style::Pallet.unpack data
 				
@@ -81,10 +80,7 @@ class StyleSystem
 				
 				
 				cascade = ThoughtTrace::Style::Cascade.new pallet
-				
 				style_names.each{ |s|  cascade.add s }
-				
-				
 				
 				obj.cascades[name] = cascade
 			end
@@ -97,58 +93,7 @@ class StyleSystem
 	
 	
 	def dump
-		# === Save all pallets
-		@pallet_list.each do |pallet|
-			name = pallet.name
-			root = pallet.project_root
-			
-			root_of_this_project = '.' # TODO: replace with real filepath logic
-			
-			# process
-			root = '__FILE__' if root == @path_to_project_root
-			
-			extension = '.yml'
-			path_to_file = File.join root, name+extension
-			
-			
-			
-			# note, the actually serialization is going to be a bit messier than this
-			# because you need to figure out how to serialize types that are not
-			# standard to Ruby, and thus not a part of YAML's type system
-			File.open(filepath, 'w') do |f|
-				YAML.dump(pallet.pack, f)
-			end
-		end
 		
-		
-		
-		
-		
-		
-		# === Save all cascades (uses information from pallets)
-		
-		# remember that cascades need to reference pallets by name and project root path
-		
-		pallet_translation_table = 
-			@pallet_list.inject(Hash.new) do |hash, pallet|
-				hash[pallet] = [pallet.name, pallet.project_root]
-				
-				hash
-			end
-		
-		@cascade_list.each do |cascade|
-			pallet_object = cascade.pallet
-			# translate object into [name, path] pair
-			
-			pallet_name, pallet_path =  pallet_translation_table[pallet_object]
-			
-			cascade_data = [pallet_path, pallet_name] + cascade.styles.to_a
-			
-			
-			File.open(filepath, 'w') do |f|
-				YAML.dump(cascade_data, f)
-			end
-		end
 	end
 	
 	class << self
