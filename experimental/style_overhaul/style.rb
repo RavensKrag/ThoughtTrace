d0c2b4f52da06e50e8ce22e584c37253cad5aeed
Raven <RavensKrag@gmail.com>
2014-08-01 21:51:24 -0400

    Basic Style object creation and binding to component.

--- experimental/style_overhaul/style.rb
+++ experimental/style_overhaul/style.rb
@@ -1,3 +1,17 @@
-class Style
+class StyleObject
+	def initialize
+		@properties = Hash.new
+	end
 	
+	def [](property)
+		return @properties[property]
+	end
+	
+	def []=(property, value) 
+		@properties[property] = value
+	end
+	
+	def has_property?(property)
+		@property.has_key? property
+	end
 end
--- experimental/style_overhaul/style_component.rb
+++ experimental/style_overhaul/style_component.rb
@@ -5,6 +5,10 @@ module Components
 class Style
 	def initialize
 		@cascade = Cascade.new
+		
+		
+		s1 = StyleObject.new
+		@cascade.add s1
 	end
 end
 
