4e166053867e158c6d36641695977f8b5efbd7b0
Raven <RavensKrag@gmail.com>
2014-08-02 13:39:22 -0400

    Sketch ideas for pallet swapping, reordering style objects in cascade, and slotting new styles into the cascade.

--- experimental/style_overhaul/sketch.rb
+++ experimental/style_overhaul/sketch.rb
@@ -33,7 +33,39 @@
 
 entity.mode = :default
 entity[:style].primary
+
 entity[:style].move 2, 4
+# select the item currently at index 2
+# insert a copy before the item at index 4
+# delete the original item at index 2
+	# arr.insert(j, arr[i]); arr.delete_at(i)
+
+entity[:style][1] = Style.new
+entity[:style][2] = other[:style].primary
+# should NOT be allowed to slot a new primary style, which resides at index 0
+
+
+
+
+
+
+
+# link styles by name
+entity[:style].add p1['first']
+
+
+# swap styles among a couple pallets
+[p1, p2, p3].each do |pallet|
+	entity[:style][1] = pallet['a']
+	entity[:style][2] = pallet['b']
+	entity[:style][3] = pallet['c']
+end
+
+
+
+
+
+
 
 
 
