c1e9f3cf6ffab85db02ae9bb31f4fb5689f5aa02
Raven <RavensKrag@gmail.com>
2014-08-01 21:49:53 -0400

    Barebones cascade.

--- experimental/style_overhaul/cascade.rb
+++ experimental/style_overhaul/cascade.rb
@@ -3,5 +3,19 @@
 # style properties can be queried though this object
 # as if you were polling one Style
 class Cascade
+	def initialize
+		@styles = Array.new
+	end
 	
+	# add a new style to the cascade
+	def add(style)
+		@styles << style
+	end
+	
+	# search cascade order for a particular property
+	def [](property)
+		# find the first style object in the cascade order which has the desired property
+		style = @styles.each.find{ |style| style.has_property? property }
+		return style[property]
+	end
 end
--- experimental/style_overhaul/style_component.rb
+++ experimental/style_overhaul/style_component.rb
@@ -3,7 +3,9 @@ module Components
 
 
 class Style
-	
+	def initialize
+		@cascade = Cascade.new
+	end
 end
 
 
