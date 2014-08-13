a43528e2cc657a068c5afe79ebfa506d13bafc03
Raven <RavensKrag@gmail.com>
2014-08-13 18:52:18 -0400

    Sketch out idea where acceleator bindings are in a ring, rather than a list. (It's actually not that great)

--- bin/rakefile.rb
+++ bin/rakefile.rb
@@ -27,7 +27,7 @@ task :run => [:build_serialization_system, :load_dependencies, :load_main] do
 	# filename ||= "default.yml"
 	# raise "No file path specified" unless filepath
 	
-	x = Window.new './data/input_bindings'
+	x = Window.new './data/input_graph'
 	x.show
 	x.on_shutdown
 end
