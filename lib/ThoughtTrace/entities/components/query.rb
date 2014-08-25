0bcbb749ab9f6ced2441ccc7d5ed30156d370a81
Raven <RavensKrag@gmail.com>
2014-08-25 10:20:05 -0400

    Start skeleton of Query object, which hooks into bind / unbind callbacks.

---
+++ lib/ThoughtTrace/entities/components/query.rb
@@ -0,0 +1,33 @@
+module ThoughtTrace
+	module Components
+
+
+
+class Query < Component
+	interface_name :query
+	components :physics, :style
+	
+	def initialize
+		
+	end
+	
+	
+	
+	
+	
+	def on_bind(entity)
+		super(entity)
+		
+		
+	end
+	
+	def on_unbind(entity)
+		super(entity)
+		
+		
+	end
+end
+
+
+end
+end
