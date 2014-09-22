860f1a1d50816a1124fc6672b56298605d41e7a4
Raven <RavensKrag@gmail.com>
2014-09-22 03:38:55 -0400

    Default query object raises warnings on callbacks, so descendants know what callbacks to define.

--- lib/ThoughtTrace/queries/query.rb
+++ lib/ThoughtTrace/queries/query.rb
@@ -9,7 +9,7 @@ class Query
 	
 	# called once when the Query first detects an Entity
 	def on_add(space, entity)
-		
+		warn "#{self.class}#on_add is not defined"
 	end
 	
 	# called every tick while the Query is aware of the Entity
@@ -17,11 +17,12 @@ class Query
 		# maybe this should be in #update?
 		# maybe #update should only be for updating the Query itself?
 		
+		warn "#{self.class}#on_tick is not defined"
 	end
 	
 	# called once when the Query first loses track of an Entity
 	def on_remove(space, entity)
-		
+		warn "#{self.class}#on_remove is not defined"
 	end
 	
 	
