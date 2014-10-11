41c89d7a1f8c7ac692a1e8738e151a5d47a7d118
Raven <RavensKrag@gmail.com>
2014-10-11 18:22:11 -0400

    clean up further, and just go from the iterator straight to the hash

--- experimental/document_format/document.rb
+++ experimental/document_format/document.rb
@@ -87,7 +87,7 @@ class Document
 		
 		
 		# generate mapping between styles and IDs
-		style_to_id_table = all_style_objects.each_with_index.to_a.to_h
+		style_to_id_table = all_style_objects.each_with_index.to_h
 		
 		
 		# --- part 2
@@ -122,7 +122,7 @@ class Document
 		
 		
 		
-		entity_to_id_table = entities.each_with_index.to_a.to_h
+		entity_to_id_table = entities.each_with_index.to_h
 		
 		
 		
