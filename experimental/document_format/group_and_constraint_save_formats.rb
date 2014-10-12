89ef6c7dc154469db149a36ded0ed28b185201e0
Raven <RavensKrag@gmail.com>
2014-10-12 15:41:19 -0400

    note that constraints are functions, not objects

--- experimental/document_format/group_and_constraint_save_formats.rb
+++ experimental/document_format/group_and_constraint_save_formats.rb
@@ -7,4 +7,6 @@ Group,1,2,3,4,5,6,7,8,9,10,11,12
 
 
 # constraints
-Constraint,2,5
+# (note that constraints are functions, not objects)
+# (you need to know what functions to call, and what entities to pass as arguments)
+constraint,2,5
