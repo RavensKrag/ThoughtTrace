21c5ed8644f8a9e97af1ebbda0ad22c2b73115ad
Raven <RavensKrag@gmail.com>
2014-10-15 13:46:32 -0400

    remove unnecessary methods

--- experimental/document_format/yaml/color.rb
+++ experimental/document_format/yaml/color.rb
@@ -15,23 +15,6 @@ class Color
 		hex_number = hex_string.to_i(16)
 		initialize(hex_number)
 	end
-	
-	
-	
-	
-	
-	# the following two methods seem to be unnecessary
-	# tested on ruby 2.1.1p76 (2014-02-24 revision 45161) [x86_64-linux] 
-	# on Ubuntu Linux 14.04, installed through RVM
-	def to_string_representation
-		self.pack.to_s(16)
-	end
-
-	def self.from_string_representation(string_representation)
-		puts "loading color"
-		hex_number = string_representation.to_i(16)
-		self.unpack(hex_number)
-	end
 end
 
 
