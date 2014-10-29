530d83a88e531edaa22d1661020dd2167f895c07
Raven <RavensKrag@gmail.com>
2014-10-29 15:55:50 -0400

    Move dependency loading into the library "header" file

--- bin/rakefile.rb
+++ bin/rakefile.rb
@@ -12,45 +12,9 @@ require 'rake/clean'
 PATH_TO_ROOT = File.expand_path '../..', __FILE__
 
 Dir.chdir PATH_TO_ROOT do
+	require './lib/ThoughtTrace'
 	# require File.expand_path File.join '.', 'lib', 'ThoughtTrace', 'serialization', 'rakefile.rb'
 end
-
-
-
-
-def load(library_path, dependency_list)
-	# Doing it this way obfuscates what is a directory, and what is a file,
-	# but it makes handling loading files in various directories
-	# (especially directories that are not children of the current directory)
-	# much easier to manage and understand.
-	Dir.chdir library_path do
-		Metrics::Timer.new "load scripts" do
-			dependency_list.each do |path|
-				begin
-					# if it's a path, require_all
-					# if it's a file, require
-					# ---------------------------
-					if File.directory? path
-						# puts "LOAD DIR: #{path}"
-						require_all path
-					# TODO: Figure out why File.file? doesn't work sans extensions like require
-					# elsif File.file? path
-					else
-						# puts "LOAD FILE: #{path}"
-						require path
-					end
-				rescue LoadError => e
-					puts "LOAD ERROR: Failed to load #{path}"
-					
-					raise e
-				end
-			end
-		
-		end
-	end
-end
-
-
 
 
 
@@ -76,87 +40,7 @@ task :build_serialization_system do
 end
 
 task :load_dependencies do
-	require 'gosu'
-	require 'gl'
 	
-	require 'DIS'
-	
-	require 'chipmunk'
-	require 'require_all'
-	
-	
-	library_root = File.join PATH_TO_ROOT, "lib", "ThoughtTrace"
-	Dir.chdir library_root do
-		require './utilities/performance_timer'
-	end
-	
-	
-	dependency_list = [
-		'./utilities/meta',
-		
-		'./monkey_patches',
-		
-		'./utilities/font',
-		'./style',
-		
-		
-		# require_all seems to snip the Class#inherited callback.
-		# wait, but only for recursive add or something?
-		# one directory at a time seems to be fine...
-		# './entities'
-		'./entities/share/',
-		'./entities/components/',
-		'./entities/',
-		
-		'./queries',
-		# './constraints',
-		'./groups',
-		
-		# './space',
-			'./space/layers',
-			'./space/queries',
-			# './space/serialization',
-			'./space/space',
-		
-		
-		'./cameras/camera',
-		
-		
-		# new actions
-		'./actions/header',
-		
-		
-		'./cloning',
-		
-		
-		# './input_system'
-			'./input_system/old/action_stash',
-			
-			'./input_system/button_event',
-			'./input_system/button_parser',
-			'./input_system/accelerator_parser',
-			'./input_system/mouse',
-			
-			'./input_system/action_factory',
-			'./input_system/mouse_input_system',
-			'./input_system/camera_controller',
-			
-			'./input_system/text_input',
-			
-			'./input_system/input_manager',
-			
-			'./input_system/custom_events',
-		
-		
-		# document format
-		'./document',
-		
-		# serialization
-		'./serialization/compiled_files',
-		'./serialization/manual_serialization',
-		'./serialization/yaml'
-	]
-	load(library_root, dependency_list)
 end
 
 task :load_main do
--- lib/ThoughtTrace.rb
+++ lib/ThoughtTrace.rb
@@ -1,3 +1,156 @@
+# This is the file that will be loaded when using `require`
+# thus, this file should be used to load up all the dependencies.
+# Think of this like a header file.
+
+
 module ThoughtTrace
-	
+# define this stuff here, just for namespacing purposes
+# don't want to overwrite some other symbols on accident
+
+
+PATH_TO_ROOT = File.expand_path '../..', __FILE__
+
+
+class << self
+
+def load_dependencies(library_path, dependency_list)
+	# Doing it this way obfuscates what is a directory, and what is a file,
+	# but it makes handling loading files in various directories
+	# (especially directories that are not children of the current directory)
+	# much easier to manage and understand.
+	Dir.chdir library_path do
+		Metrics::Timer.new "load scripts" do
+			dependency_list.each do |path|
+				begin
+					# if it's a path, require_all
+					# if it's a file, require
+					# ---------------------------
+					if File.directory? path
+						# puts "LOAD DIR: #{path}"
+						require_all path
+					# TODO: Figure out why File.file? doesn't work sans extensions like require
+					# elsif File.file? path
+					else
+						# puts "LOAD FILE: #{path}"
+						require path
+					end
+				rescue LoadError => e
+					puts "LOAD ERROR: Failed to load #{path}"
+					
+					raise e
+				end
+			end
+		
+		end
+	end
 end
+
+
+end
+end
+
+
+
+
+
+
+
+
+
+
+
+require 'gosu'
+require 'gl'
+
+require 'DIS'
+
+require 'chipmunk'
+require 'require_all'
+
+
+library_root = File.join ThoughtTrace::PATH_TO_ROOT, "lib", "ThoughtTrace"
+Dir.chdir library_root do
+	require './utilities/performance_timer'
+end
+
+
+dependency_list = [
+	'./utilities/meta',
+	
+	'./monkey_patches',
+	
+	'./utilities/font',
+	'./style',
+	
+	
+	# require_all seems to snip the Class#inherited callback.
+	# wait, but only for recursive add or something?
+	# one directory at a time seems to be fine...
+	# './entities'
+	'./entities/share/',
+	'./entities/components/',
+	'./entities/',
+	
+	'./queries',
+	# './constraints',
+	'./groups',
+	
+	# './space',
+		'./space/layers',
+		'./space/queries',
+		# './space/serialization',
+		'./space/space',
+	
+	
+	'./cameras/camera',
+	
+	
+	# new actions
+	'./actions/header',
+	
+	
+	'./cloning',
+	
+	
+	# './input_system'
+		'./input_system/old/action_stash',
+		
+		'./input_system/button_event',
+		'./input_system/button_parser',
+		'./input_system/accelerator_parser',
+		'./input_system/mouse',
+		
+		'./input_system/action_factory',
+		'./input_system/mouse_input_system',
+		'./input_system/camera_controller',
+		
+		'./input_system/text_input',
+		
+		'./input_system/input_manager',
+		
+		'./input_system/custom_events',
+	
+	
+	# document format
+	'./document',
+	
+	# serialization
+	'./serialization/compiled_files',
+	'./serialization/manual_serialization',
+	'./serialization/yaml'
+]
+ThoughtTrace.load_dependencies(library_root, dependency_list)
+
+
+
+
+
+
+
+
+
+
+
+puts "root: #{ThoughtTrace::PATH_TO_ROOT}"
+
+# ThoughtTrace.load_dependencies()
