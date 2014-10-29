2d9a506c033219cde0d0de1f5d76cb484a8afd2b
Raven <RavensKrag@gmail.com>
2014-10-29 16:23:08 -0400

    add some notes about the 'run' file

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+
+# note: This file is just a shortcut for the actual rakefile that drives the program. It just makes it easier for other people to be able to start up the program.
 path_to_file = File.expand_path(File.dirname(__FILE__))
 Dir.chdir path_to_file
 
