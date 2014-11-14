be2e37e25038ae0fc14defdadd8b355b7072096c
Raven <RavensKrag@gmail.com>
2014-11-13 23:13:34 -0500

    sketch a basic constraint, to draw a line between two entities

--- lib/ThoughtTrace/constraints/constraint.rb
+++ lib/ThoughtTrace/constraints/constraint.rb
@@ -3,9 +3,49 @@ module ThoughtTrace
 
 
 class Constraint
-	def initialize
-		
+	# are constraints always pairwise?
+	# not sure if I can even make that call right now
+	
+	
+	# bind constraint to entity objects
+	def initialize(*entity_list)
+		@entities = entity_list
 	end
+	
+	
+	# apply one tick of the constraint
+	def update
+		raise "Constraint #{self.class} does not implement #update"
+	end
+	
+	# visualize the current state of the constraint
+	def draw
+		raise "Constraint #{self.class} does not implement #draw"
+	end
+	
+	
+	
+	
+	
+	
+	
+	# ===== serialization =====
+	# convert ONE object to / from array on pack / unpack
+	def pack
+		return @entities.clone
+	end
+	
+	
+	class << self
+		def unpack(*entities)
+			group = self.new
+			
+			entities.each{ |e| group.add e  }
+			
+			return group
+		end
+	end
+	# =========================
 end
 
 
