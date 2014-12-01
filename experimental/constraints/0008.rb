c5f167d86bd5025f71bcf5cfea773b2917d84dbd
Raven <RavensKrag@gmail.com>
2014-11-30 22:16:46 -0500

    maybe you need to mark all the changed entities in one pass, and then apply constraint in another?

--- experimental/constraints/0008.rb
+++ experimental/constraints/0008.rb
@@ -31,28 +31,51 @@ cache[entity] = p[entity]
 
 
 def save(entity)
-	@cache[entity] = foo(entity)
+	@entity = entity
+	@data   = foo(entity)
 end
 
 def load(entity)
-	@cache[entity]
+	@data
+end
+
+def same_entity?(entity)
+	@entity == entity
 end
 
 def changed?(entity)
 	data = load(entity)
 	
 	if data == nil
+		# constraint has not even run once yet
 		save(entity)
 		return true
 	else
-		return data != foo(entity)
+		# this constraint instance has run at least once
+		if same_entity?(entity)
+			# running on the same entity as last time
+			new_data = save(entity)
+			return data != new_data
+		else
+			# trying to run on an entity other than the one to which we are bound
+			# return false so that we do not select this entity
+			return false
+		end
 	end
 end
 
 
 
+# accept parameters that can be used to alter the #call step
+def initialize
+	
+end
+
 # list the things that will be changed
 # needed to figure out when the entities are changing
+
+# dependency, prerequisite?
+# (is it a concurrent dependency? - interrelated, correlated)
 def foo(entity)
 	[
 		entity[:physics].height
@@ -83,9 +106,71 @@ def filter
 	return p
 end
 
+
+
+
+# PropagatingConstraint
+def choose(a,b)
+	return a
+end
+
+# LimitingConstraint
+def choose(a,b)
+	return b
+end
+
+
+
 # NOTE: with the current implementation of #changed? and how it related to #filter, only one constraint will be able to fire off of a change in a particular Entity
 
 
 
 selection = @entities.select &constraint.filter
 selection.each{ |a,b|  constraint.call(a,b)  }
+
+
+
+
+
+
+
+
+
+
+all_pairs do |a,b|
+	call(a,b) if filter.call(a,b)
+end
+
+
+
+
+
+
+
+
+
+
+# try filtering the results this way, so you can catch multiple items
+# not sure if this is actually necessary, considering how much the entity list will be constrained
+
+# go through and mark everything that may have changed,
+# and then 
+# (this implementation assumes the hash backend caching is used. be careful, or the caches could bloat very quicky)
+
+# NOTE: @entities is assumed to hold the entities relevant to this constraint, not ALL entities
+require 'set'
+monad = Monad.new(@entities)
+
+# select only relevant entities that have changed since the last time the constraint was run
+# NOTE: need to revert some changed to #changed?
+# constraint ticks are defined pairwise, but constraints may track more than a pair of entity objects
+changed_set = @entities.select{ |x| constraint.changed?(x)  }.to_set
+
+selection = monad.all_pairs.select{ |a,b|
+				changed_set.include? constraint.choose(a,b)
+			}
+
+
+selection.each{ |a,b|  constraint.call(a,b)  }
+
+
