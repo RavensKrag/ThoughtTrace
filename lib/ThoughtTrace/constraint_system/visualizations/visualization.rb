b7aefb7165143b46ea10fcde5e0c53145b1f8586
Raven <RavensKrag@gmail.com>
2014-12-04 03:32:00 -0500

    Visualization should be able to deal with the wrapping time counter

--- lib/ThoughtTrace/constraint_system/visualizations/visualization.rb
+++ lib/ThoughtTrace/constraint_system/visualizations/visualization.rb
@@ -64,15 +64,20 @@ class Visualization
 	# wait 3 sec
 	# do some other stuff
 	class Timer
+		# NOTE: this will not work on JRuby. But I can't currently deploy for that anyway.
+		# src: https://bugs.ruby-lang.org/issues/7517
+		MAX_FIXNUM = 1 << (1.size * 8 - 2) - 1
+		
+		
 		def initialize
 			@start_time = nil
-			@time = 0
+			@target_time = 0
 		end
 		
 		def update
-			if now - @start_time >= @time
+			if elapsed_time(@start_time, @target_time) >= @target_time
 				if @block
-					@block.call 
+					@block.call
 					@block = nil
 				end
 			end
@@ -80,7 +85,7 @@ class Visualization
 		
 		def wait(time, &block)
 			@start_time = now()
-			@time = time
+			@target_time = time
 			@block = block
 		end
 		
@@ -88,13 +93,22 @@ class Visualization
 		
 		# returns an integer timestamp for the current time (ms resolution)
 		def now
+			# NOTE: Gosu::milliseconds will always return a Fixnum
 			Gosu::milliseconds
 		end
 		
 		# compute the time elapsed between two millisecond timestamps
 		# takes into account possibility of the timer wrapping back around
 		def elapsed_time(start, stop)
-			
+			if start < stop
+				# standard
+				return stop - start
+			else
+				# wrap-around has occurred
+					# measure from start to max, and from 0 to stop
+					# (start..max) + (0..stop)
+				return (MAX_FIXNUM - start) + (stop)
+			end
 		end
 	end
 	
