8327ceed546b8e640fdf89227a15abab911cc454
Raven <RavensKrag@gmail.com>
2015-02-02 22:05:26 -0500

    sketch out a bit of the entity marker

--- lib/ThoughtTrace/constraint_system/NEW/marker.rb
+++ lib/ThoughtTrace/constraint_system/NEW/marker.rb
@@ -1,5 +1,22 @@
 class EntityMarker
 	def initialize
+		# create dummy entity object
+		# null object pattern for when the marker has nothing to track
+		@@dummy ||= x
+	end
+	
+	def update
+		# move position to match the position of the tracked Entity, if any
+	end
+	
+	def draw
+		
+	end
+	
+	
+	
+	
+	def bind_to(entity)
 		
 	end
 end
