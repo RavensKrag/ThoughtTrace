63a30ad2302915c3709211e2f24a21b28821d61d
Raven <RavensKrag@gmail.com>
2015-02-06 22:16:18 -0500

    add skeleton of action to move constraint marker

---
+++ lib/ThoughtTrace/constraint_system/NEW/move_marker.rb
@@ -0,0 +1,145 @@
+class EntityMarker
+	module Actions
+
+
+class Move < ThoughtTrace::Entity::Actions::Move
+	@type_list = Entity::Actions::Move.argument_type_list
+	# by creating a child class of an action, you inherit the initializer, but not the type list
+	
+	
+	
+	# called on first tick
+	def setup(point)
+		super(point)
+	end
+	
+	# called each tick
+	def update(point)
+		# move relative to the initial point
+		current = super(point)
+		
+		
+		
+		
+		
+		return current
+	end
+	
+	# not often used, but you can define this callback if you need it
+	# really, just added for completeness
+	def cleanup(point)
+		
+	end
+	
+	
+	# display information to the user about the current transformation
+	# called each tick
+	def draw(point)
+		
+	end
+	
+	
+	
+	
+	# perform the transformation here
+	# by encapsulating the transform in this object,
+	# it becomes easy to redo / undo actions as necessary
+	ParentMemento = self.superclass.const_get 'Memento'
+	class Memento < ParentMemento
+		# set future state
+		def forward
+			super()
+		end
+		
+		# set past state
+		def reverse
+			super()
+		end
+	end
+end
+
+
+
+end
+end
+
+
+
+
+module ThoughtTrace
+	class Entity
+		module Actions
+
+
+class Move < Entity::Actions::Action
+	initialize_with :entity
+	
+	# called on first tick
+	def setup(point)
+		# mark the initial point for reference
+		@origin = point
+		
+		# mark the initial point for reference
+		@origin = point
+		@start = @entity[:physics].body.p.clone
+		
+		return @start
+	end
+	
+	# called each tick
+	def update(point)
+		# move relative to the initial point
+		current = @entity[:physics].body.p = @start + movement_delta(point)
+		
+		
+		return current
+	end
+	
+	# not often used, but you can define this callback if you need it
+	# really, just added for completeness
+	def cleanup(point)
+		
+	end
+	
+	
+	# display information to the user about the current transformation
+	# called each tick
+	def draw(point)
+		
+	end
+	
+	
+	
+	private
+	
+	def movement_delta(point)
+		return point - @origin
+	end
+	
+	public
+	
+	
+	
+	
+	# perform the transformation here
+	# by encapsulating the transform in this object,
+	# it becomes easy to redo / undo actions as necessary
+	ParentMemento = self.superclass.const_get 'Memento'
+	class Memento < ParentMemento
+		# set future state
+		def forward
+			@entity[:physics].body.p = @future
+		end
+		
+		# set past state
+		def reverse
+			@entity[:physics].body.p = @initial
+		end
+	end
+end
+
+
+
+end
+end
+end
