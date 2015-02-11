91b01845ad9b7f67d2afed81308c877561a7dfe3
Raven <RavensKrag@gmail.com>
2015-02-11 16:03:30 -0500

    notes on the  weirdness of naming this class Enumerator

--- lib/ThoughtTrace/constraint_system/enumerators/enumerator.rb
+++ lib/ThoughtTrace/constraint_system/enumerators/enumerator.rb
@@ -3,6 +3,12 @@ module ThoughtTrace
 		module Enumerators
 
 
+# NOTE: maybe don't name this Enumerator?
+
+# class is named Enumerator
+# because it is a special kind of... enumerator
+# but there is a core class called Enumerator
+# and simply using name-spacing alone to distinguish them makes it really easy to get the wrong one
 class Enumerator
 	# TODO: expand functionality to mirror the standard Enumerator class
 	def initialize(entity_list)
