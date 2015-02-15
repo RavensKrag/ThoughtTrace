08d30d0132ef6b9fe252edb1f2741a4ce6275f8e
Raven <RavensKrag@gmail.com>
2015-02-14 20:58:25 -0500

    commit old notes on 'how do I get old state' now that a solution has been commited

--- lib/ThoughtTrace/constraint_system/constraints/move_relative.rb
+++ lib/ThoughtTrace/constraint_system/constraints/move_relative.rb
@@ -24,7 +24,19 @@ class MoveRelative
 
 	# execute one tick
 	def call(a,b)
-		# how do I get the previous value?
+		# TODO: how do I get the previous value? Need to know what the position of B was after the end of the last tick of this constraint. Can't currently store in in the instance variables of this constraint object, as the constraint object is shared between multiple constraint pairs.
+		# but it should be noted that I built the system this way because I assumed that there would need to be stored state within a constraint. If that assumption was incorrect, maybe some things need to be reconsidered.
+		
+		# could take the parameter closure out of the constraint, and store in in the Package, I suppose?
+		
+		
+		
+		# need to figure out if you need some arbitrary state tracking, of if you only need to track the data in the cache (both this state, and the previous state)
+		# if you only need cache data, this is pretty simple
+		# if you need arbitrary state permanence, then this is gonna get complicated
+		# (I think you only need the cache data, but need to make sure)
+				
+		
 		prev = 
 		delta = b[:physics].body.p - prev
 		
