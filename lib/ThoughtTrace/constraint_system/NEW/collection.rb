7ca34a93475227603a167954aa4c25601fc9e5dc
Raven <RavensKrag@gmail.com>
2015-02-14 19:49:16 -0500

    update collection to use new Package structure

--- lib/ThoughtTrace/constraint_system/NEW/collection.rb
+++ lib/ThoughtTrace/constraint_system/NEW/collection.rb
@@ -12,8 +12,10 @@ class Collection
 	# currently assuming constraints are method objects
 	# yes. method. objects.
 	# take the containing object and call #method(name) to retrieve the method object
-	def add(constraint, visualization)
-		@list << ConstraintPackage.new(constraint, visualization)
+	def add(package)
+		# package could be a ConstraintPackage, or it could just be a raw Constraint
+		# (the latter case is an optimization, not a "normal" thing)
+		@list << package
 	end
 	
 	def update
@@ -24,7 +26,8 @@ class Collection
 	
 	def draw
 		@list.each do |package|
-			package.draw
+			# raw Constraint objects don't have visualizations, so not all objects will #draw
+			package.draw if package.respond_to? :draw
 		end
 	end
 	
@@ -55,46 +58,7 @@ class Collection
 		collection = self.new
 		
 		
-		data_dump.each do |constraint_data|
-			# constraint_data
-			constraint, visualization, a,b = constraint_data
-			
-			
-			
-			a = [
-				ThoughtTrace::Constraints,
-				ThoughtTrace::Constraints::Visualizations
-			]
-			
-			b = [constraint, visualization]
-			
-			c = 
-				a.zip(b).collect do |container, const_name|
-					klass = container.const_get const_name
-				end
-			
-			
-			# THIS IS WEIRD
-			# DON'T ACTUALLY DO THIS
-			# don't do this in deployment code
-			
-			# I didn't want the initialization to happen here,
-			# I wanted instances of classes to be loaded into the Collection
-			# so like... the data that is serialized I guess should be instances?
-			# c[0] = c[0].new
-			
-			
-			
-			
-			d = [:constraint, :enumerator, :visualization].zip(c).to_h
-			
-			constraint    = d[:constraint].new # DON'T ACTUALLY DO THIS IN PRODUCTION CODE
-			enumerator    = d[:enumerator].new(entity_list)
-			visualization = d[:visualization].new
-			
-			
-			collection.add constraint, enumerator, visualization
-		end
+		
 		
 		return collection
 	end
