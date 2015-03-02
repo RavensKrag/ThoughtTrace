b9e50cbd1ee0e57d8d893939874135ceda28dabd
Raven <RavensKrag@gmail.com>
2015-03-02 12:36:14 -0500

    clear up comments on MoveRelative constraint

--- lib/ThoughtTrace/constraint_system/constraints/move_relative.rb
+++ lib/ThoughtTrace/constraint_system/constraints/move_relative.rb
@@ -25,19 +25,8 @@ class MoveRelative < Constraint
 	# should not mutate cache, should not store any state in the Constraint object
 	# (the Constraint object is shared between many Pairs)
 	def call(a,b, cache) # @closure
-		# TODO: how do I get the previous value? Need to know what the position of B was after the end of the last tick of this constraint. Can't currently store in in the instance variables of this constraint object, as the constraint object is shared between multiple constraint pairs.
-		# but it should be noted that I built the system this way because I assumed that there would need to be stored state within a constraint. If that assumption was incorrect, maybe some things need to be reconsidered.
-		
-		# could take the parameter closure out of the constraint, and store in in the Package, I suppose?
-		
-		
-		
-		# need to figure out if you need some arbitrary state tracking, of if you only need to track the data in the cache (both this state, and the previous state)
-		# if you only need cache data, this is pretty simple
-		# if you need arbitrary state permanence, then this is gonna get complicated
-		# (I think you only need the cache data, but need to make sure)
-		
-		return if cache.prev.nil?
+		return if cache.prev.nil? # cache doesn't always have a 'prev' value set
+		# NOTE: this may be while this constraint is one frame behind
 		
 		
 		prev = cache.prev[0]
