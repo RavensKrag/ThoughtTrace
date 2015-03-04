1aa59a3ba67875875ac07a740c50c0716cbd95f4
Raven <RavensKrag@gmail.com>
2015-03-04 15:46:40 -0500

    update null action

--- lib/ThoughtTrace/actions/null_action.rb
+++ lib/ThoughtTrace/actions/null_action.rb
@@ -19,20 +19,54 @@ class NullAction < BaseAction
 	
 	
 	
-	def setup(point)
+	# called on first tick
+	def press(point)
 		puts "#{@entity} -> setup null "
 	end
 	
+	# called each tick after the first tick (first tick is setup only)
+	# perform calculations to generate the new data, but don't change the data yet.
+	# Many ticks of #update can be generated before the final application is decided.
 	def update(point)
 		puts "#{@entity} -> update null"
 	end
 	
-	def cleanup(point)
-		puts "#{@entity} -> cleanup null"
+	# Actually apply changes to data.
+	# Called after #update on each tick, and also on redo.
+	# Many ticks of #apply can be fired before the action completes.
+	def apply
+		puts "#{@entity} -> apply null"
 	end
 	
-	def cancel
-		puts "#{@entity} -> cancel null"
+	# restore original state
+	# revert the changes made by all ticks of #apply
+	# (some actions need to store state to make this work, other actions can fire an inverse fx)
+	def undo
+		puts "#{@entity} -> undo null"
+	end
+	
+	# final tick of the Action
+	# (used to be called #cleanup)
+	def release(point)
+		puts "#{@entity} -> release null"
+	end
+	
+	
+	
+	
+	
+	
+	
+	# NOTE: Action visualizations are not the same as Constraint visualizations
+	def update_visualization(point)
+		
+	end
+	
+	
+	# display information to the user about the current transformation
+	# called each tick
+	def draw
+		
 	end
 end
 
