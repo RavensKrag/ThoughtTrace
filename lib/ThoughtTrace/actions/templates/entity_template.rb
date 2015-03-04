4a0f4f7f3917c3015c48e88ea788f486273a2dca
Raven <RavensKrag@gmail.com>
2015-03-04 15:43:26 -0500

    update entity action template

--- lib/ThoughtTrace/actions/templates/entity_template.rb
+++ lib/ThoughtTrace/actions/templates/entity_template.rb
@@ -12,49 +12,56 @@ class Foo < Entity::Actions::Action
 	# metaprogrraming method
 		# defines #initialize to set specified values
 		# defines list of symbol names, so Action creation system knows what variables to pass in
-
+	
 	
 	# called on first tick
-	def setup(point)
-		@original = nil
-		return @original
+	def press(point)
+		
 	end
 	
-	# called each tick
+	# called each tick after the first tick (first tick is setup only)
+	# perform calculations to generate the new data, but don't change the data yet.
+	# Many ticks of #update can be generated before the final application is decided.
 	def update(point)
 		
-		return current
 	end
 	
-	# not often used, but you can define this callback if you need it
-	# really, just added for completeness
-	def cleanup(point)
+	# Actually apply changes to data.
+	# Called after #update on each tick, and also on redo.
+	# Many ticks of #apply can be fired before the action completes.
+	def apply
+		
+	end
+	
+	# restore original state
+	# revert the changes made by all ticks of #apply
+	# (some actions need to store state to make this work, other actions can fire an inverse fx)
+	def undo
+		
+	end
+	
+	# final tick of the Action
+	# (used to be called #cleanup)
+	def release(point)
+		
+	end
+	
+	
+	
+	
+	
+	
+	
+	# NOTE: Action visualizations are not the same as Constraint visualizations
+	def update_visualization(point)
 		
 	end
 	
 	
 	# display information to the user about the current transformation
 	# called each tick
-	def draw(point)
+	def draw
 		
-	end
-	
-	
-	
-	# perform the transformation here
-	# by encapsulating the transform in this object,
-	# it becomes easy to redo / undo actions as necessary
-	ParentMemento = self.superclass.const_get 'Memento'
-	class Memento < ParentMemento
-		# set future state
-		def forward
-			@entity.baz(@future)
-		end
-		
-		# set past state
-		def reverse
-			@entity.baz(@initial)
-		end
 	end
 end
 
