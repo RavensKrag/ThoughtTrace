287a988e1a4dea7a06a148497023b41a4983dd86
Raven <RavensKrag@gmail.com>
2015-03-06 16:02:47 -0500

    update text edit action to work with new action format

--- lib/ThoughtTrace/actions/entity/text/edit.rb
+++ lib/ThoughtTrace/actions/entity/text/edit.rb
@@ -8,42 +8,35 @@ class Edit < Entity::Actions::Action
 	
 	# called on first tick
 	def press(point)
-		@inital = [@text_input, @clone_factory]
+		@point = point
 	end
 	
 	# called each tick after the first tick (first tick is setup only)
 	# perform calculations to generate the new data, but don't change the data yet.
 	# Many ticks of #update can be generated before the final application is decided.
 	def update(point)
-		@future = point
+		
 	end
 	
 	# Actually apply changes to data.
 	# Called after #update on each tick, and also on redo.
 	# Many ticks of #apply can be fired before the action completes.
 	def apply
-		text_input, clone_factory = @initial
-		point = @future
-		
-		
-		text_input.add @entity, @entity.nearest_character_boundary(point)
+		@text_input.add @entity, @entity.nearest_character_boundary(@point)
 		
 		
 		# NOTE: Can't move prototype registration into TextInput because you would have no way to step back from that transformation in this Action.
 		# TODO: Figure out how to move prototype registration into TextInput, rather than this one action.
-		@old_prototype = clone_factory.make ThoughtTrace::Text
-		clone_factory.register_prototype @entity
+		@old_prototype = @clone_factory.make ThoughtTrace::Text
+		@clone_factory.register_prototype @entity
 	end
 	
 	# restore original state
 	# revert the changes made by all ticks of #apply
 	# (some actions need to store state to make this work, other actions can fire an inverse fx)
 	def undo
-		text_input, clone_factory = @initial
-		point = @future
-		
-		text_input.clear
-		clone_factory.register_prototype @old_prototype
+		@text_input.clear
+		@clone_factory.register_prototype @old_prototype
 	end
 	
 	# final tick of the Action
