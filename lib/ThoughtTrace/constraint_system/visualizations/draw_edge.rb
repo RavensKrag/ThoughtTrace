c0787d4837bb55859c0323d473299c2a0662e6fe
Raven <RavensKrag@gmail.com>
2015-03-16 08:49:08 -0400

    remove obselete comments

--- lib/ThoughtTrace/constraint_system/visualizations/draw_edge.rb
+++ lib/ThoughtTrace/constraint_system/visualizations/draw_edge.rb
@@ -22,8 +22,6 @@ class DrawEdge < Visualization
 		end
 	end
 	
-	# TODO: consider having two separate objects for active and inactive states, so that the two states can keep their data completely separate
-	# TODO: consider that only one visualization object needs to be made - this wrapper - and that the inside classes could be something else? or maybe that those objects should be the visualization classes, and this wrapper should be called something else
 	def draw(a,b)
 		color = @style['color']
 		
