a0c4d041eb85b0815d1479eab5ada3bbc3756efc
Raven <RavensKrag@gmail.com>
2015-03-18 17:07:59 -0400

    change Entity Collection to use primarily an Array, with a Hash for caching

--- experimental/entity_collection.rb
+++ experimental/entity_collection.rb
@@ -10,18 +10,15 @@ class Collection
 		@space = space
 		
 		@obj_to_index = Hash.new
-		@index_to_obj = Hash.new
-		
-		@next_i = 0
+		@index_to_obj = Array.new
 	end
 	
 	# fast ish? don't really care
 	def add(object)
-		i = @next_i
-		@index_to_obj[i] = object
+		i = @index_to_obj.size
 		@obj_to_index[object] = i
 		
-		@next_i += 1
+		@index_to_obj << object
 	end
 	
 	# don't care
@@ -29,7 +26,7 @@ class Collection
 		i = @obj_to_index[object]
 		
 		@obj_to_index.delete object
-		@index_to_obj.delete i
+		@index_to_obj[i] = nil
 	end
 	
 	# swap the items at the two indicies given
@@ -74,7 +71,7 @@ class Collection
 		# 
 		# I implemented #gc in order to help fix that issue,
 		# but having to remember to GC regularly could be it's own problem
-		@next_i.times do |i|
+		@index_to_obj.size.times do |i|
 			obj = @index_to_obj[i]
 			next if obj.nil?
 			
@@ -100,44 +97,10 @@ class Collection
 	
 	
 	# Compress the range of used indicies, such that there are no gaps.
+	# By simply putting 'nil's into the Array, instead of doing a 'real' delete,
+	# you can just move a bunch of things every once in a while.
 	# 
-	# ie) after running #gc, all indicies in the range [0..@next_i) will be occupied
-	# ( Although, that may be confusing, as @next_i will be reset as well )
-	def gc
-		# create a list of valid indicies, in order from low to high
-		index_list = @next_i.times.select{|i|  @index_to_obj[i]  }
-		
-		
-		# remap that onto a compressed range, with no gaps
-		obj_list = index_list.collect{|i|  @index_to_obj[i] }
-			
-			# clear out the maps, so you don't get any weird garbage data left over
-			# 
-			# note that if the value if @next_i decrease at the end of this operation,
-			# there would be indicies that correspond to duplicate data.
-			# @obj_to_index would be ok, but @index_to_object will have issues
-			@index_to_obj.clear
-			# @obj_to_index.clear
-			
-		obj_list.each_with_index do |obj, i|
-			@index_to_obj[i] = object
-			@obj_to_index[object] = i
-		end
-		@next_i = obj_list.size
-		
-		
-		# alternative clean up method to using #clear
-		# @index_to_obj.delete_if{ |i,obj|  i >= @next_i }
-		# another alternative. ugly, but more efficient
-		# ((@index_to_obj.size)..(@next_i-1)).each{|i|  @index_to_obj.delete i }
-		
-		
-		raise "Remapping failed" unless @index_to_obj.size == @obj_to_index.size
-	end
-	
-	
-	# alternative gc sketch: assuming the @index_to_obj collection is an array
-	# NOTE: Assumed that on delete you just put a 'nil' into the array, instead of moving everything. This way, you can just move a bunch of things every once in a while.
+	# NOTE: #gc can potentially create garbage in the @index_to_obj collection, because the number of indicies is decreasing. But the @obj_to_index collection will never have more entries than necessary, because the number of objects before and after #gc is constant.
 	def gc
 		# do a non-in-place operation in attempt to shrink C-Level data store
 		@index_to_obj = @index_to_obj.compact
@@ -146,11 +109,6 @@ class Collection
 		@index_to_obj.each_with_index do |obj, i|
 			@obj_to_index[object] = i
 		end
-		
-		# @next_i = @index_to_obj.size
-		# Don't need the @next_i variable any more, because can just append to the end of the list.
-		# That would have been a problem in a list where the length constantly shifts,
-		# but it's totally fine in this scenario.
 		
 		raise "Remapping failed" unless @index_to_obj.size == @obj_to_index.size
 	end
