fc4b12103f7c820f48b3be362a94baf74d1897c5
Raven <RavensKrag@gmail.com>
2015-03-19 23:09:53 -0400

    Actions to add / subtract from Selection based on copy / pasted code

--- lib/ThoughtTrace/actions/selection/selection_subtract.rb
+++ lib/ThoughtTrace/actions/selection/selection_subtract.rb
@@ -10,14 +10,15 @@ class SelectionSubtract < ThoughtTrace::Actions::BaseAction
 	
 	# called on first tick
 	def press(point)
-		
+		@select = @action_factory.create(nil, :select)
+		@select.press(point)
 	end
 	
 	# called each tick after the first tick (first tick is setup only)
 	# perform calculations to generate the new data, but don't change the data yet.
 	# Many ticks of #update can be generated before the final application is decided.
 	def update(point)
-		
+		@select.update(point)
 	end
 	
 	# Actually apply changes to data.
@@ -27,18 +28,38 @@ class SelectionSubtract < ThoughtTrace::Actions::BaseAction
 		
 	end
 	
+	# final tick of the Action
+	# (used to be called #cleanup)
+	def release(point)
+		@set = @select.release(point)
+		@old_selection = @selection.collect{|x|  x }
+		
+		# selection should really be a Group, not just a Set
+		# also, you can't just set the value by setting the variable: you need to move the data
+		@set.each do |entity|
+			@selection.delete entity
+		end
+	end
+	
 	# restore original state
 	# revert the changes made by all ticks of #apply
 	# (some actions need to store state to make this work, other actions can fire an inverse fx)
 	def undo
-		
+		@selection.clear
+		@old_selection.each do |entity|
+			@selection.add entity
+		end
 	end
 	
-	# final tick of the Action
-	# (used to be called #cleanup)
-	def release(point)
-		
+	# changed my mind about undo-ing
+	# apply the originally intended transformation to the data
+	def redo
+		@set.each do |entity|
+			@selection.delete entity
+		end
 	end
+	
+	
 	
 	
 	
@@ -48,14 +69,14 @@ class SelectionSubtract < ThoughtTrace::Actions::BaseAction
 	
 	# NOTE: Action visualizations are not the same as Constraint visualizations
 	def update_visualization(point)
-		
+		@select.update_visualization(point)
 	end
 	
 	
 	# display information to the user about the current transformation
 	# called each tick
 	def draw
-		
+		@select.draw
 	end
 end
 
