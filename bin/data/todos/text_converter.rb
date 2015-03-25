6b764320231e2ce9c6604cfd47c0f40a82e6a4f9
Raven <RavensKrag@gmail.com>
2015-03-24 22:16:59 -0400

    add various todos to graph

--- bin/data/todos/text_converter.rb
+++ bin/data/todos/text_converter.rb
@@ -1,53 +1,29 @@
+require 'rubygems'
+require 'chipmunk'
+
 x = "
-selection - group - group actions - disambiguation
-group system - mark and select
-clear selection on non-selection-altering action?
-groups / copy group
-pick group / abstraction layer with scroll wheel
-TODO: consider ActionFactory#get_type for groups
-all groups should start off as selections
-NOTE: constraint Visualizations and Groups now both use Style Components
-component serialization - non-Entity types
-implement loading of visualizations
-NOTE: each Constraint Pair needs a separate Visualization object
-Visualizations need to save Style component data
-rethink how Style data is saved
-what do you do about deleting Entity markers?
-document format - multiple documents
-nested document
-nested document - cascade to resolve action
-layers of interaction (for UI etc)
-TODO: limit camera drawing
-TODO: camera layers
-TODO: camera should be spatial data
-constraint creation action
-constraint / constraint package maker
-API to create / delete whole Constraint Packages
-add / remove whole entities (including components)
-Entity delete mechanism
-input system overhaul
-input binding - no-target actions
-input binding - mode-specific UI interaction
-notes program - tree overview - text field w/ spell check
-multi-line text input
-how do you bind mouse click actions for a Entity?
-action system documentation update
-TODO: rewrite 'empty space actions' as 'zero target actions'
-move on to queries
-implement prefabs
-implement text split-rip action
-try to implement something like text caret again
-TODO: awkward to see the text caret appear quickly - then vanish
-locking things in place
-attachment point markers
-query object serialization
-bind query object to query component
-how are Queries shown visually?
-implement text search of all Text objects
-multiple editor views
-short-circuiting min_by -> really a lazy min_by
-TODO: make sure Physics#center works with all collision shapes ASAP
-shrink rect resize margins as object shrinks
+TODO: track system - query style binding
+TODO: track system development - serialization
+\"so, what about things that apply data to certain entities?\"
+NOTE: potential problems with propagating constraints
+constraints on multiple objects
+\"NOTE: space is trying to call #gc? on constraints, but method is not yet defined\"
+TODO: list current constraint backend objects
+BUG: object packer breaks when trying to generate pack/unpack for an object with a 0 arg init
+\"Q: do you need to click on Constraints, not just markers?\"
+TODO: all markers in a document should use the same visual style
+\"TODO: make sure Action#update ticks the same frame as #press, otherwise there may be a one frame lag on single-tick actions (notably, spawn actions)\"
+TODO: consider action factory / mouse action extraction system interaction with multiple spaces
+TODO: polymorphic edit and resize
+\"TODO: consider scrapping rect resize action, in favor of constraint-based control system\"
+\"TODO: update rectangle resize #anchor_point to accept an argument, instead of mucking with state\"
+TODO: action factory should not use ThoughtTrace::Queries::Query check
+BUG: crash on marker move
+NOTE: general debug flow
+BUG: rect resize action counter-steering wrecks LimitHeight
+TODO: consider inverting LimitHeight
+\"TODO: track time per frame ms, and convert to hz\"
+\"TODO: load up both a limiting and a propagating constraint, to test Action / Constraint interaction\"
 "
 
 y = x.each_line.to_a; y.shift
@@ -55,6 +31,9 @@ y = x.each_line.to_a; y.shift
 z = y.collect{|x| x.chomp}
 
 
+height = 23
+p = CP::Vec2.new(1356.2500000000002,-306.22564972180544)
+
 z.each_with_index do |x,i|
-	puts "ThoughtTrace::Text,Lucida Sans Unicode,0.0,#{23*i},22.97394578559605,#{x}"
+	puts "ThoughtTrace::Text,Lucida Sans Unicode,#{p.x},#{p.y+height*i},#{height},#{x}"
 end
