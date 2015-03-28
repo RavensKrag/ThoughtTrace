0766d0442547a539f3426d4e803ec4758a1567e0
Raven <RavensKrag@gmail.com>
2015-03-27 20:52:03 -0400

    another batch of old notes

--- bin/data/todos/text_converter.rb
+++ bin/data/todos/text_converter.rb
@@ -2,16 +2,21 @@ require 'rubygems'
 require 'chipmunk'
 
 x = "
-many spawn actions
-think ahead into selection
-editor workflow versus core coding backend interface
-why did I need queries the last time I wanted queries?
-insert feels like a special kind of move
-are insert and join the same thing?
-TODO: consider action factory / mouse action extraction system interaction with multiple spaces
-TODO: consider overhaul of input system
-button up /  down
-specific accelerator combo vs any accelerators
+improve mouseover state
+May want to change how text resizes horizontally on center drag
+\"If Style is created without a parent, it's parent should default to the baseline style\"
+Figure out how to change Entities when style properties are altered
+edit space point query
+is there any reason to keep Component?
+TODO: implement screen space to world space conversion in accordance with new systems
+TODO: custom subclass of Hash to deal with action bindings
+TODO: Ruby methods are objects. Apply structure to Action
+TODO: consider different views of same space
+TODO: change the way corner resize of rectangles works
+\"TODO: fix short circuiting min by, and the entities/text.rb -> Text#nearest_character_boundary() optimization that it messes up\"
+TODO: create action to delete a whole Entity
+TODO: remove Object delegate metaprogramming monkeypatch
+TODO: make sure an empty project can be loaded
 "
 
 y = x.each_line.to_a; y.shift
@@ -20,7 +25,7 @@ z = y.collect{|x| x.chomp}
 
 
 height = 23
-p = CP::Vec2.new(1814.5833333333333,-941.6423163884722)
+p = CP::Vec2.new(2860.416666666667,-691.6423163884722)
 
 z.each_with_index do |x,i|
 	puts "ThoughtTrace::Text,Lucida Sans Unicode,#{p.x},#{p.y+height*i},#{height},#{x}"
