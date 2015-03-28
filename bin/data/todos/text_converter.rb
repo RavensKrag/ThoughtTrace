9f95494fd0170bedc1dde44e85bf8f78f259daa9
Raven <RavensKrag@gmail.com>
2015-03-27 20:37:28 -0400

    even more old notes

--- bin/data/todos/text_converter.rb
+++ bin/data/todos/text_converter.rb
@@ -2,21 +2,16 @@ require 'rubygems'
 require 'chipmunk'
 
 x = "
-image Entity type
-non-destructive crop of Images
-multi-item mask (not just images)
-load initial prototype config from base config for entire library
-action - input - query
-action - input - constraint
-sharper font scaling
-input system - how inputs trigger actions
-diagram showing all actions
-collision handler system
-mulitple phases of text input
-TODO: create graph relating files to subsystems
-list ALL the THINGS
-TODO: adjust body masses and collisions
-NOTE: notice that the prototype for Text keeps getting updated
+many spawn actions
+think ahead into selection
+editor workflow versus core coding backend interface
+why did I need queries the last time I wanted queries?
+insert feels like a special kind of move
+are insert and join the same thing?
+TODO: consider action factory / mouse action extraction system interaction with multiple spaces
+TODO: consider overhaul of input system
+button up /  down
+specific accelerator combo vs any accelerators
 "
 
 y = x.each_line.to_a; y.shift
@@ -25,7 +20,7 @@ z = y.collect{|x| x.chomp}
 
 
 height = 23
-p = CP::Vec2.new(1393.75,213.56601694486142)
+p = CP::Vec2.new(1814.5833333333333,-941.6423163884722)
 
 z.each_with_index do |x,i|
 	puts "ThoughtTrace::Text,Lucida Sans Unicode,#{p.x},#{p.y+height*i},#{height},#{x}"
