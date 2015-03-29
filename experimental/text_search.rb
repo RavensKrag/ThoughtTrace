5fd53fea20a5575dc48f34e670ae494b3ee95ee1
Raven <RavensKrag@gmail.com>
2015-03-29 08:52:51 -0400

    basic sketch of searching for some string, and then highlighting what you find

--- experimental/text_search.rb
+++ experimental/text_search.rb
@@ -5,12 +5,32 @@ def search(target_string)
 	text_objects.select!{ |text|  text.string.include? target_string }
 	
 	
-	# highlight the portions of the string the match the search query
-	collection = Array.new text_objects.size
+	highlight_mapping = 
+		text_objects.map do |text|
+			i = text.string.index target_string
+			
+			[text, i, i+target_string.size] if i
+		end
+	highlight_mapping.compact!
 	
-	text_objects.each_with_index do |text, i|
-		collection[i] = Highlight.new
-		collection[i]
+	
+	
+	# highlight the portions of the string the match the search query
+	color = Gosu::Color.argb(0xaaFF0000)
+	
+	highlight_mapping.each do |text, start_i, end_i|
+		# NOTE: width_of_first may break for i=1, because it handles conversion weird. need to totally overhaul that
+		ax = text.width_of_first(start_i)
+		bx = text.width_of_first(end_i)
+		
+		p = text[:physics].body.p
+		a = CP::Vec2.new(ax, 0) + p
+		b = CP::Vec2.new(bx, 0) + p
+		
+		ThoughtTrace::Drawing.draw_line(
+			render_context, a,b, 
+			color:color, thickness:6, line_offset:0.5, z_index:0
+		)
 	end
 end
 
