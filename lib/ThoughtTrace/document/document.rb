ea5765ae53efdaacda9c10d9653e82c580fa0797
Raven <RavensKrag@gmail.com>
2015-03-29 08:58:08 -0400

    load up rudamentary text search highlighting into document

--- lib/ThoughtTrace/document/document.rb
+++ lib/ThoughtTrace/document/document.rb
@@ -127,6 +127,9 @@ class Document
 			$window.flush
 			@constraint_packages.draw
 			yield
+			
+			
+			# text_search("constraint")
 		end
 	end
 	
@@ -138,6 +141,47 @@ class Document
 	
 	def gc
 		@space.gc
+	end
+	
+	
+	
+	
+	# find text objects with the desired string inside
+	def text_search(target_string)
+		text_objects = @space.entities.select{|x| x.is_a? ThoughtTrace::Text }
+		text_objects.select!{ |text|  text.string.include? target_string }
+		
+		
+		highlight_mapping = 
+			text_objects.map do |text|
+				i = text.string.index target_string
+				
+				[text, i, i+target_string.size] if i
+			end
+		highlight_mapping.compact!
+		
+		
+		
+		# highlight the portions of the string the match the search query
+		color = Gosu::Color.argb(0x99FF0000)
+		
+		highlight_mapping.each do |text, start_i, end_i|
+			# NOTE: width_of_first may break for i=1, because it handles conversion weird. need to totally overhaul that
+			ax = text.width_of_first(start_i)
+			bx = text.width_of_first(end_i)
+			
+			height = text[:physics].shape.height
+			offset = height / 2
+			
+			p = text[:physics].body.p
+			a = CP::Vec2.new(ax, offset) + p
+			b = CP::Vec2.new(bx, offset) + p
+			
+			ThoughtTrace::Drawing.draw_line(
+				$window, a,b, 
+				color:color, thickness:height, line_offset:0.5, z_index:0
+			)
+		end
 	end
 	
 	
