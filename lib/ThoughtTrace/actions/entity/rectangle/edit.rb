5cc07d57a54d6b7d7eaadaccf8d4d1747b741eee
Raven <RavensKrag@gmail.com>
2015-04-05 20:14:17 -0400

    First draft of new vert-moving no aspect ratio lock rectangle 'edit' Action

--- lib/ThoughtTrace/actions/entity/rectangle/edit.rb
+++ lib/ThoughtTrace/actions/entity/rectangle/edit.rb
@@ -20,175 +20,154 @@ class Edit < ThoughtTrace::Actions::BaseAction
 		# ASSUMPTION: point is already known to lie within the shape
 		
 		
-		# convert to local space
-		# find distance to edges using local x,y coordinate system
-			# don't need to actually use distance formula,
-			# and can get distance even if there's rotation in the shape.
-		top_right = @entity[:physics].shape.top_right_vert
-		bottom_left = @entity[:physics].shape.bottom_left_vert
+		
+		@original_verts = @entity[:physics].shape.verts
+		
+		# origin top left, x+ right, y+ down,
+		# assuming one of the verts is on the origin, and shape stretches in the positive direction
+		# (standard structure of Rect shape)
+		@original_width  = @entity[:physics].shape.width
+		@original_height = @entity[:physics].shape.height
 		
 		
 		local_point = @entity[:physics].body.world2local point
+		m = MARGIN
+		w = @original_width
+		h = @original_height
+		
+		# xL = 0
+		xa = m
+		xb = w-m
+		xR = w
+		
+		# yT = 0
+		ya = m
+		yb = h-m
+		yB = h
 		
 		
-		
-		
-		# Figure out what sector out of nine the point is in
-		# (top, bottom, right, left, top_right, top_left, bottom_right, bottom_left, center)
-		x =	if local_point.x.between? top_right.x - MARGIN, top_right.x
-				# :right
-				1
-			elsif local_point.x.between? bottom_left.x, bottom_left.x + MARGIN
-				# :left
+		x = 
+			if    local_point.x < xa
+				# left
 				-1
-			else
+			elsif local_point.x < xb
+				# center
 				0
+			elsif local_point.x < xR
+				# right
+				puts "right"
+				1
 			end
 		
-		y =	if local_point.y.between? top_right.y - MARGIN, top_right.y
-				# :top
-				1
-			elsif local_point.y.between? bottom_left.y, bottom_left.y + MARGIN
-				# :bottom
+		y = 
+			if    local_point.y < ya
+				# top
+				puts "top"
 				-1
-			else
+			elsif local_point.y < yb
+				# center
 				0
+			elsif local_point.y < yB
+				# bottom
+				1
 			end
+		
+		# different edge characteristics,
+		# slicing from low to high vs chopping the edges from the core
+		# only would really effect very small parts of the data, but it's something to think about
+		
 		
 		@direction = CP::Vec2.new(x,y)
 		
 		
+		# verts are specified [tl, tr, br, bl] in +x right y+ up space
+		# bl,br,tr,tl @entity[:physics].shape.verts
 		
-		# Reshape diagonals to point at the actual corners,
-		# not just the corners of an ideal square
-		if x != 0 && y != 0
-			@direction.x *= @entity[:physics].shape.width
-			@direction.y *= @entity[:physics].shape.height
-			
-			@direction.normalize!
-		end
+		# bitmask for what verts should be changed
+		@vert_bitmask = 
+			case [x,y]
+				when [-1, -1]
+					0b1000
+				when [ 0, -1]
+					0b1100
+				when [ 1, -1]
+					0b0100
+				
+				when [-1,  0]
+					0b1001
+				when [ 0,  0]
+					0b0000
+				when [ 1,  0]
+					0b0110
+				
+				when [-1,  1]
+					0b0001
+				when [ 0,  1]
+					0b0011
+				when [ 1,  1]
+					0b0010
+			end
+		# wait this is too general of a solution.
+		# the moving of the edge allows for the creation of shapes which are no longer rectangles.
+		# That is not what you want.
+		# You need to enforce that edges can only move along one axis,
+		# while verts can move on two axes.
 		
-		
-		
-		
-		shape = @entity[:physics].shape
-		@original_width = shape.width
-		@original_height = shape.height
+		@direction.normalize!
 	end
 	
 	# called each tick after the first tick (first tick is setup only)
 	# perform calculations to generate the new data, but don't change the data yet.
 	# Many ticks of #update can be generated before the final application is decided.
 	def update(point)
-		local_origin = @entity[:physics].body.world2local @origin
-		local_point = @entity[:physics].body.world2local point
-		local_displacement = local_point - local_origin
+		displacement = point - @origin
+		
+		# projection = displacement.project(@direction)
+		
+		
+		
+		if    [0b1001, 0b0110].include? @vert_bitmask
+			# x axis only (left or right edge)
+			displacement.y = 0
+		elsif [0b0011, 0b1100].include? @vert_bitmask
+			# y axis only (top or bottom edge)
+			displacement.x = 0
+		else
+			# movement on both axes
+			# single vert
 			
-			
-			return if local_displacement.zero? # short circuit when there is no movement
-			
-			
-			# only use the component of the displacement in the direction of the edited component
-			# ie) the direction of a corner, or one of the edges
-			# this is NOT currently the value of the @direction vector
-			# that merely shows which edges should be scaled
-			# thus, the current implementation scales corners faster
-				# (diagonal straight-line distance is shorter than taxi-cab distance)
-			
-			# get axes
-			width = @original_width
-			height = @original_height
-			
-			if @direction.zero?
-				# ===== Radial Scaling =====
-				# scale about the center
-				
-				# Sign-age of scale is relative to center of rectangle
-				# towards center is negative (shrinking)
-				# away from center is positive (growing)
-				
-				
-				# --- Magnitude of transform
-				# find vector starting from center, and going towards the current point
-				center = @entity[:physics].shape.center
-				center_to_point = local_point - center
-				radial_axis = center_to_point.normalize
-				
-				
-				
-				# displacement in local space along the radial vector
-				radial_displacement = local_displacement.project(radial_axis).length
-				
-				# flip sign to negative if necessary
-				same_direction = local_displacement.dot(radial_axis) > 0
-				radial_displacement *= -1 unless same_direction
-				
-				
-				
-				
-				# --- Apply magnitude of transform in appropriate directions
-				# multiply by two, because resizing is happening in two directions at once
-				width  += radial_displacement * 2
-				height += radial_displacement * 2
-			else
-				# ===== Cartesian Scaling =====
-				# scale along the axes of the rectangle
-				
-				# pin down part (edge or vert) of the rectangle, and stretch out the rest
-				
-				# rescale in the direction specified by @direction
-				# displacement towards the center of the shape is negative,
-				# displacement towards the outside of the shape is positive
-				
-				projection = local_displacement.project(@direction)
-				
-				
-				# Compute new dimensions
-				if projection.x != 0
-					# Horizontal Stretch
-					
-					if @direction.x < 0
-						width -= projection.x
-					else
-						width += projection.x
-					end
-				end
-				if projection.y != 0
-					# Vertical Stretch
-					
-					if @direction.y < 0
-						height -= projection.y
-					else
-						height += projection.y
-					end
-				end
+		end
+		
+		
+		# 0b0000
+		#   8421
+		bitmask = @vert_bitmask
+		verts = @original_verts.collect{ |x|  x.clone }
+		verts.each_with_index do |vert, i|
+			if bitmask & (1 << i) != 0 # check if bit at position i is set
+				vert.x += displacement.x
+				vert.y += displacement.y
 			end
-			
-			
-			
-			# limit minimum size (like a clamp, but lower bound only)
-			width  = MINIMUM_DIMENSION if width  < MINIMUM_DIMENSION
-			height = MINIMUM_DIMENSION if height < MINIMUM_DIMENSION
+		end
 		
-		
-		
-		@width = width
-		@height = height
-		@anchor = anchor_point()
+		@new_verts = verts
 	end
 	
 	# Actually apply changes to data.
 	# Called after #update on each tick, and also on redo.
 	# Many ticks of #apply can be fired before the action completes.
 	def apply
-		@entity.resize!(@width, @height, @anchor)
+		# @entity.resize!(@width, @height, @anchor)
+		
+		offset = CP::Vec2.new(0,0)
+		@entity[:physics].shape.set_verts! @new_verts, offset
 	end
 	
 	# restore original state
 	# revert the changes made by all ticks of #apply
 	# (some actions need to store state to make this work, other actions can fire an inverse fx)
 	def undo
-		@entity.resize!(@original_width, @original_height, @anchor)
+		# @entity.resize!(@original_width, @original_height, @anchor)
 		# use the same anchor from the apply step.
 		# this should be enough to reverse the operation.
 		# There's no real way to calculate an "inverse anchor",
