1e3d838a87107beb359287b32171d9dea91877aa
Raven <RavensKrag@gmail.com>
2015-04-27 22:21:27 -0400

    rearrange methods

--- lib/ThoughtTrace/monkey_patches/Chipmunk/Shape/Rect.rb
+++ lib/ThoughtTrace/monkey_patches/Chipmunk/Shape/Rect.rb
@@ -14,6 +14,22 @@ module CP
 			
 			
 			
+		# verts named in "x+ right, y+ up" coordinate space
+		[:top_left, :top_right, :bottom_right, :bottom_left].each_with_index do |corner, i|
+			define_method "#{corner}_vert" do
+				self.vert(i)
+			end
+		end
+			
+			
+			# returns the center of this shape, in local space
+			def center
+				top_right_vert / 2
+			end
+			
+			
+			
+			
 			# TODO: Make sure @offset is ok this way. It may have to become a relative offset system to get things to resize dynamically as intended. (related to the anchor point system used in Rectangle entity [rectangle anchor]). Come back and check on this when Constraints are more developed. May not work as intended.
 			
 			def width
@@ -30,22 +46,6 @@ module CP
 				new_verts = new_geometry(width, height)
 				
 				self.set_verts! new_verts, @offset
-			end
-			
-			
-			
-			
-		# verts named in "x+ right, y+ up" coordinate space
-		[:top_left, :top_right, :bottom_right, :bottom_left].each_with_index do |corner, i|
-			define_method "#{corner}_vert" do
-				self.vert(i)
-			end
-		end
-		
-			
-			# returns the center of this shape, in local space
-			def center
-				top_right_vert / 2
 			end
 			
 			
