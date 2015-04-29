d6de820f13f7e5b3ced79bd99da51cf4ee26a9b0
Raven <RavensKrag@gmail.com>
2015-04-29 02:19:22 -0400

    nice first draft of rectangle resize action

--- lib/ThoughtTrace/actions/entity/rectangle/resize.rb
+++ lib/ThoughtTrace/actions/entity/rectangle/resize.rb
@@ -14,6 +14,7 @@ class Resize < ThoughtTrace::Rectangle::Actions::Edit
 	# called on first tick
 	def press(point)
 		super(point)
+		@origin = point
 	end
 	
 	# called each tick after the first tick (first tick is setup only)
@@ -21,13 +22,144 @@ class Resize < ThoughtTrace::Rectangle::Actions::Edit
 	# Many ticks of #update can be generated before the final application is decided.
 	def update(point)
 		super(point)
+		@delta = point - @origin
 	end
 	
 	# Actually apply changes to data.
 	# Called after #update on each tick, and also on redo.
 	# Many ticks of #apply can be fired before the action completes.
 	def apply
-		super()
+		undo()
+		
+		
+		type, target_indicies = CP::Shape::Rect::VEC_TO_TRANSFORM_DATA[@grab_handle.to_a]
+		
+		# p target_indicies
+		
+		new_verts = @entity[:physics].shape.verts
+		
+		case type
+			when :edge
+				# store original dimensions before any transforms
+				width  = @entity[:physics].shape.width
+				height = @entity[:physics].shape.height
+				
+				
+				
+				
+				# scale the edge along the axis shared by it's verts
+				a,b = target_indicies.collect{|i| new_verts[i] }
+				axis = ( a.x == b.x ? :x : :y )
+				
+				
+				
+				minimum = self.class.superclass::MINIMUM_DIMENSION
+				@entity[:physics].shape.resize_to_point!(@grab_handle, @point, minimum)
+				
+				
+				
+				# TODO: consider possible problems of dividing delta by two. Should you use integer division? The underlying measurements are pixels, so what happens when you divide in half? Will you ever lose precision?
+				
+				# TODO: minimum dimension for aspect ratio locked scaling needs to take into account aspect ratio, or else on the extreme low end, you ignore aspect ratio and converge to square.
+				
+				
+				# then, scale along the other axis
+				if axis == :x
+					# scale along y
+					original_width = width
+					width  = @entity[:physics].shape.width
+					
+					ratio = width.to_f / original_width.to_f
+					new_height = height * ratio
+					
+					delta = new_height - height
+					
+					
+					a = CP::Vec2.new(0,  1)
+					b = CP::Vec2.new(0, -1)
+					@entity[:physics].shape.resize_by_delta!(a, a*delta/2, minimum)
+					@entity[:physics].shape.resize_by_delta!(b, b*delta/2, minimum)
+				else # axis == :y
+					# scale along x
+					original_height = height
+					height = @entity[:physics].shape.height
+					
+					ratio = height.to_f / original_height.to_f
+					new_width = width * ratio
+					
+					delta = new_width - width
+					
+					
+					a = CP::Vec2.new( 1, 0)
+					b = CP::Vec2.new(-1, 0)
+					@entity[:physics].shape.resize_by_delta!(a, a*delta/2, minimum)
+					@entity[:physics].shape.resize_by_delta!(b, b*delta/2, minimum)
+				end
+				
+				
+				
+			when :vert
+				center = @entity[:physics].shape.center
+				vert = new_verts[target_indicies.first]
+				diagonal = (vert - center).normalize
+				
+				vec = @delta.project(diagonal)
+				
+				minimum = self.class.superclass::MINIMUM_DIMENSION
+				@entity[:physics].shape.resize_by_delta!(@grab_handle, vec, minimum)
+				
+				
+			when :center
+				# nothing
+		end
+		
+		
+		
+		# offset = new_verts[3] * -1
+		# # this vert is by default (0,0) in local space,
+		# # so you need to restore it to it's default position as the local origin.
+		# # if you don't, then width / height calculations get weird
+		
+		# @entity[:physics].shape.set_verts!(new_verts, offset)
+		# @entity[:physics].shape.body.p -= offset
+		
+		
+		# # then, scale along the other axis
+		# midpoint = CP::Vec2.midpoint(a,b)
+		# midpoint_world = @entity[:physics].body.local2world(midpoint)
+			
+		# 	# list = [:x, :y]
+		# 	# list.delete(axis)
+		# 	# scale_axis = list.first
+		# 	p verts.collect{|p| p.to_s}
+		# 	if axis == :x
+		# 		# scale along y
+		# 		original_width = width
+		# 		width += @delta.x
+				
+		# 		ratio = width.to_f / original_width.to_f
+		# 		height = height * ratio
+				
+				
+		# 		verts[0].y = height
+		# 	else # axis == :y
+		# 		# scale along x
+		# 		original_height = height
+		# 		height += @delta.y
+				
+		# 		ratio = height.to_f / original_height.to_f
+		# 		width = width * ratio
+				
+				
+		# 		verts[2].x = width
+		# 	end
+		
+		# a,b = target_indicies.collect{|i| verts[i] }
+		# new_midpoint = CP::Vec2.midpoint(a,b)
+		# new_midpoint_world = @entity[:physics].body.local2world(new_midpoint)
+		
+		# shift = new_midpoint_world - midpoint_world
+		# @entity[:physics].body.p += shift
 	end
 	
 	# restore original state
@@ -61,52 +193,6 @@ class Resize < ThoughtTrace::Rectangle::Actions::Edit
 		# TODO: draw margins to get a better idea of how they should be altered as the shape changes.
 		# TODO: consider implementing margin rendering using entities and constraints. Then that data could easily be used to drive the modulation of the margins themselves.
 		super()
-	end
-	
-	
-	
-	
-	
-	
-	private
-	
-	def cartesian_scaling(point, delta, width, height)
-		# ===== Cartesian Scaling =====
-		# scale along the axes of the rectangle
-		
-		# pin down part (edge or vert) of the rectangle, and stretch out the rest
-		
-		# rescale in the direction specified by @direction
-		# displacement towards the center of the shape is negative,
-		# displacement towards the outside of the shape is positive
-		
-		
-		
-		delta = delta.project(@direction)
-		
-		
-		
-		
-		original_width  = width
-		original_height = height
-		
-		width, height = super(point, delta, width, height)
-		
-		
-		# aspect ratio lock for edge scaling
-		# (corner aspect ratio lock is a systemic consequence of projection onto @direction)
-		if    @direction.x == 0 and @direction.y != 0
-			# vertical scaling
-			ratio = height.to_f / original_height.to_f
-			width = width * ratio
-		elsif @direction.x != 0 and @direction.y == 0
-			# horizontal scaling
-			ratio = width.to_f / original_width.to_f
-			height = height * ratio
-		end
-		
-		
-		return width,height
 	end
 end
 
