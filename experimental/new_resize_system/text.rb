fc056c71e0462ec96f4210c26ef87afba5316299
Raven <RavensKrag@gmail.com>
2015-05-26 07:18:11 -0400

    sketch for new text resize method

---
+++ experimental/new_resize_system/text.rb
@@ -0,0 +1,30 @@
+class Text < Rectangle
+	def resize!(grab_handle, coordinate_space=nil, point:nil, delta:nil, minimum_dimension:1, lock_aspect:false, limit_by:nil)
+		
+		# save
+		original_verts    = self[:physics].shape.verts
+		original_position = self[:physics].body.p.clone
+		
+		# process
+			# resize backend rect with fixed aspect ratio, to get a good guess of the size
+			@entity[:physics].shape.resize!(
+				@grab_handle, :world_space, point:@point, lock_aspect:true,
+				minimum_dimension:MINIMUM_FONT_HEIGHT, limit_by: :height
+			)
+			
+			# Perform final, exactly height computation on the final tick ONLY.
+			# Performing this every tick causes a lot of jitter.
+			@entity.height = @entity[:physics].shape.height
+			# (setting height this way allows Text to set the exact width)
+		
+		
+		# return proc to reverse the process		
+		undo = Proc.new do
+			# same as for Rectangle
+			self[:physics].shape.set_verts!(original_verts, CP::Vec2.new(0,0))
+			self[:physics].body.p = original_position
+		end
+		
+		return undo
+	end
+end
