6c457be5bc73249c01c8996e7cca56fda9f80a87
Raven <RavensKrag@gmail.com>
2015-05-26 18:03:34 -0400

    try to actually use the Text sketch stuff in the live Action

--- lib/ThoughtTrace/actions/entity/text/resize.rb
+++ lib/ThoughtTrace/actions/entity/text/resize.rb
@@ -11,7 +11,8 @@ class Resize < Rectangle::Actions::Resize
 	
 	# called on first tick
 	def press(point)
-		super(point) # sets @origin and @direction
+		super(point)
+		@origin = point
 	end
 	
 	# called each tick after the first tick (first tick is setup only)
@@ -22,6 +23,7 @@ class Resize < Rectangle::Actions::Resize
 		
 		# simple ratio solution courtesy of this link
 		# http://tech.pro/tutorial/691/csharp-tutorial-font-scaling
+		@delta = point - @origin
 	end
 	
 	# Actually apply changes to data.
@@ -30,12 +32,86 @@ class Resize < Rectangle::Actions::Resize
 	def apply
 		# NOTE: Want to always limit the minimum HEIGHT on resize. Don't really care about what the width is. This applies to Text only, not general rectangles.
 		
+		# grab_handle = @grab_handle
+		# minimum_dimension = MINIMUM_FONT_HEIGHT
+		# point = @point
 		
-		# resize backend rect with fixed aspect ratio, to get a good guess of the size
+		
+		undo()
+		
+		counter_steer_anchor = ->(grab_handle){
+			countersteer_handle = grab_handle * -1
+			x = countersteer_handle.to_a
+			type, target_indidies = CP::Shape::Rect::VEC_TO_TRANSFORM_DATA[x]
+			
+			
+			local_anchor = 
+				case type
+					when :edge
+						target_indidies
+							.collect{  |i|    @entity[:physics].shape.vert(i)    }
+							.reduce{   |a,b|  CP::Vec2.midpoint(a,b)                 }
+					when :vert
+						i = target_indidies.first
+						@entity[:physics].shape.vert(i)
+					else
+						return # short-circuit when you attempt to use action on center
+				end
+			
+			return local_anchor
+		}
+		
+		
+		
+		
+		
+		
+		# prep for counter-steering
+		local_anchor = counter_steer_anchor[@grab_handle]
+		
+		anchor = @entity[:physics].body.local2world(local_anchor)
+		
+		
+		# set width and height
+		type, target_indidies = CP::Shape::Rect::VEC_TO_TRANSFORM_DATA[@grab_handle.to_a]
+		
+		origin =
+			case type
+				when :edge
+					target_indidies
+						.collect{  |i|    @entity[:physics].shape.vert(i)    }
+						.reduce{   |a,b|  CP::Vec2.midpoint(a,b)                 }
+				when :vert
+					i = target_indidies.first
+					@entity[:physics].shape.vert(i)
+			end
+		
+		dh = @delta.y
+		dh *= -1 if @grab_handle.y < 0
+		height = @entity.height + dh
+		
+		
+		# NOTE: currently resizes correctly on height, but not on width (because the delta is currently only looking at the change in the y direction). But the core logic is currently set around picking a height, so that's why.
+		
+		
+		height = height.round # rounding needs to happen somewhere to prevent jitter. this works.
+		
+		h = [height, MINIMUM_FONT_HEIGHT].max
+		w = @entity.font.width(@entity.string, h)
+		
+		grab_handle = CP::Vec2.new(1,1)
+		point       = CP::Vec2.new(w,h)
+		
 		@entity[:physics].shape.resize!(
-			@grab_handle, :world_space, point:@point, lock_aspect:true,
-			minimum_dimension:MINIMUM_FONT_HEIGHT, limit_by: :height
+			grab_handle, :local_space, point:point, lock_aspect:false
 		)
+		
+		
+		
+		# counter-steer
+		local_anchor = counter_steer_anchor[@grab_handle]
+		@entity[:physics].right_hand_on_red(local_anchor, anchor)
+			# NOTE: what you really want is to save the local anchor in normalized space, so that you can use the exact same anchor twice. but that still requires the computer to calculate what the local anchor is in NEW non-normalized space. So even though that would be more humanistic, it may be worse for performance. (also floating point errors)
 	end
 	
 	# restore original state
@@ -49,11 +125,6 @@ class Resize < Rectangle::Actions::Resize
 	# (used to be called #cleanup)
 	def release(point)
 		super(point)
-		
-		# Perform final, exactly height computation on the final tick ONLY.
-		# Performing this every tick causes a lot of jitter.
-		@entity.height = @entity[:physics].shape.height
-		# (setting height this way allows Text to set the exact width)
 	end
 	
 	
