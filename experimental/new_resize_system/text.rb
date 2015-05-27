0660b4714f0321f0d5265376a931e20b209b55bf
Raven <RavensKrag@gmail.com>
2015-05-27 02:31:02 -0400

    update sketch with latest code

--- experimental/new_resize_system/text.rb
+++ experimental/new_resize_system/text.rb
@@ -7,23 +7,7 @@ class Text < Rectangle
 		original_position = self[:physics].body.p.clone
 		
 		# process
-			# === THIS IS THE OLD GLITCHY WAY
-			# resize backend rect with fixed aspect ratio, to get a good guess of the size
-			@entity[:physics].shape.resize!(
-				grab_handle, coordinate_space, point:point, lock_aspect:true,
-				minimum_dimension:minimum_dimension, limit_by: :height
-			)
-			
-			# Perform final, exactly height computation on the final tick ONLY.
-			# Performing this every tick causes a lot of jitter.
-			@entity.height = @entity[:physics].shape.height
-			# (setting height this way allows Text to set the exact width)
-			
-			
-			
-			
-			# === TRY THIS WAY INSTEAD
-			# prep for counter-steering
+		counter_steer_anchor = ->(grab_handle){
 			countersteer_handle = grab_handle * -1
 			x = countersteer_handle.to_a
 			type, target_indidies = CP::Shape::Rect::VEC_TO_TRANSFORM_DATA[x]
@@ -33,47 +17,72 @@ class Text < Rectangle
 				case type
 					when :edge
 						target_indidies
-							.collect{  |i|    @components[:physics].shape.vert(i)    }
+							.collect{  |i|    @entity[:physics].shape.vert(i)    }
 							.reduce{   |a,b|  CP::Vec2.midpoint(a,b)                 }
 					when :vert
 						i = target_indidies.first
-						@components[:physics].shape.vert(i)
+						@entity[:physics].shape.vert(i)
+					else
+						return # short-circuit when you attempt to use action on center
 				end
 			
-			anchor = @components[:physics].body.local2world(local_anchor)
-			
-			
-			# set width and height
-			type, target_indidies = CP::Shape::Rect::VEC_TO_TRANSFORM_DATA[grab_handle.to_a]
-			
-			origin =
-				case type
-					when :edge
-						target_indidies
-							.collect{  |i|    @components[:physics].shape.vert(i)    }
-							.reduce{   |a,b|  CP::Vec2.midpoint(a,b)                 }
-					when :vert
-						i = target_indidies.first
-						@components[:physics].shape.vert(i)
-				end
-			
-			d = point - origin
-			dh = d.y
-			dh *= -1 if grab_handle.y < 0
-			height = self.height + d.y
-			
-			
-			h = [height, minimum_dimension].max
-			w = @font.width(@string, height)
-			
-			grab_handle = CP::Vec2.new(1,1)
-			point       = CP::Vec2.new(w,h)
-			@components[:physics].shape.resize!(
-				grab_handle, :local_space, point:point, lock_aspect:false
-			)
-			
-			# counter-steer
-			@components[:physics].right_hand_on_red(local_anchor, anchor)
+			return local_anchor
+		}
+		
+		
+		
+		
+		# === prep for counter-steering
+		local_anchor = counter_steer_anchor[@grab_handle]
+		return unless local_anchor
+		anchor = @entity[:physics].body.local2world(local_anchor)
+		
+		
+		# === set width and height
+		# resize the hitbox, and use that to figure out what the final height should be
+		# (but going to resize and re-position AGAIN before final render, so no one will see this)
+		@entity[:physics].shape.resize!(
+			@grab_handle, :world_space, point:@point, lock_aspect:true,
+			minimum_dimension:MINIMUM_FONT_HEIGHT, limit_by: :height
+		)
+		
+		height = @entity[:physics].shape.height
+		
+		# It doesn't feel like you're dragging along the diagonal, as with Rectangle resize.
+		# It feels like you're just resizing the width, and then the height changes to match
+		# (older Text resize code actually did that, and both old and new have the same behavior)
+		# But this new code can be written in terms of scaling to a specific point,
+		# where as the old one could only be written in terms of deltas.
+		
+		
+		
+		# NOTE: it's important to remember that when specifying a width, you can't always get the width you want. The height of the font, the font face, and the number / type of characters in the string constrain what widths are possible.
+		
+		
+		height = height.round # rounding needs to happen somewhere to prevent jitter. this works.
+		# In addition to jitter, it is possible for the hitbox to be sized narrower than the text,
+		# which is very very odd.
+		
+		
+		# NOTE: need to set height again to apply rounding.
+		h = height
+		# puts h
+		w = @entity.font.width(@entity.string, h)
+		# puts w
+		
+		grab_handle = CP::Vec2.new(1,1)
+		point       = CP::Vec2.new(w,h)
+		
+		@entity[:physics].shape.resize!(
+			grab_handle, :local_space, point:point, lock_aspect:false
+		)
+		
+		
+		
+		# === counter-steer
+		local_anchor = counter_steer_anchor[@grab_handle]
+		@entity[:physics].right_hand_on_red(local_anchor, anchor)
+			# NOTE: what you really want is to save the local anchor in normalized space, so that you can use the exact same anchor twice. but that still requires the computer to calculate what the local anchor is in NEW non-normalized space. So even though that would be more humanistic, it may be worse for performance. (also floating point errors)
 			
 			
 			
